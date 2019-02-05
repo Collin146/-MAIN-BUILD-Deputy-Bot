d8d3e65213ef68a6ce4aa8d5789ea72777fb60e6
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-05 17:02:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -82,24 +82,6 @@ bot.on("message", async message => {
 
 });
 
-if(cmd === `{$prefix}userinfo`){
-    let member = message.mentions.users.first() || message.author;
-    let userembed = new Discord.RichEmbed()
-        .setColor(message.guild.member(member).highestRole.color)
-        .setThumbnail(member.displayAvatarURL)
-        
-        .setTitle(`Here is ${member.username}'s info.`)
-        .addField(`Name:`, member.username, true)
-        .addField(`Id:`, member.id, true)
-        .addField(`Bot:`, member.bot ? "Yes" : "No", true)
-        .addField("Game:", message.guild.member(member).presence.game ? message.guild.member(member).presence.game.name : "Not Playing", true) // the ? and : are like an if statement if (msg.guild.member(member).presence.game ) { msg.guild.member(member).presence.game.name } else "Not Playing"
-        .addField("Nickname:", message.guild.member(member).nickname ? message.guild.member(member).nickname : "None", true )
-        .addField("Last Messsage:", member.lastMessage, true)
-        .addField(`Roles:`, message.guild.member(member).roles.map(s => s).join(" | "), true)
-
-        message.channel.send(userembed);
-}
-
 bot.on('error', err => {
     console.log(err);
 });
