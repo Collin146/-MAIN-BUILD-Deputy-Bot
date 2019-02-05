33b7b790f63a976d13da6b19a2c6865c6e62fdf6
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-05 17:02:39 +0100

    Create userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+    if(args[0] === "help"){
+        message.reply("Usage: !userinfo <user>");
+        return;
+    }
+
+    let member = message.mentions.users.first() || message.author;
+    let userembed = new Discord.RichEmbed()
+        .setColor(message.guild.member(member).highestRole.color)
+        .setThumbnail(member.displayAvatarURL)
+        
+        .setTitle(`Here is ${member.username}'s info.`)
+        .addField(`Name:`, member.username, true)
+        .addField(`Id:`, member.id, true)
+        .addField(`Bot:`, member.bot ? "Yes" : "No", true)
+        .addField("Game:", message.guild.member(member).presence.game ? message.guild.member(member).presence.game.name : "Not Playing", true) // the ? and : are like an if statement if (msg.guild.member(member).presence.game ) { msg.guild.member(member).presence.game.name } else "Not Playing"
+        .addField("Nickname:", message.guild.member(member).nickname ? message.guild.member(member).nickname : "None", true )
+        .addField("Last Messsage:", member.lastMessage, true)
+        .addField(`Roles:`, message.guild.member(member).roles.map(s => s).join(" | "), true)
+
+        message.channel.send(userembed);
+}
+
+module.exports.help = {
+    name: "userinfo"
+}
