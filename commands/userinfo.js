cb4a61da6eeceaaae7ae31286818c892577edc7f
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-05 01:50:53 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,22 +1,8 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => { 
-
-const status = {
-  online: "Online",
-  idle: "Idle",
-  dnd: "Do Not Disturb",
-  offline: "Offline/Invisible"
-};
-const randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); });
-  const member = msg.mentions.members.first() || msg.guild.members.get(args[0]) || msg.member;
-  if (!userinfo) return msg.reply("Please provide a vaild Mention or USER ID");
-  if (member.user.bot === true) {
-    bot = "Yes";
-  } else {
-    bot = "No";
-  }
-  const embed = new Discord.MessageEmbed()
+module.exports.run = async (bot, message, args) => {
+    const member = msg.mentions.members.first() || msg.guild.members.get(args[0]) || msg.member;
+    const embed = new Discord.MessageEmbed()
     .setColor(randomColor)
     .setThumbnail(`${member.user.displayAvatarURL()}`)
     .setAuthor(`${member.user.tag} (${member.id})`, `${member.user.avatarURL()}`)
@@ -30,21 +16,10 @@ const randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random
     .addField("Created At", `${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true);
 
   msg.channel.send({embed});
-};
 
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ["uinfo"],
-  permLevel: 0
-};
-
-exports.help = {
-  name: "userinfo",
-  description: "Gets userinfo from a mention or id",
-  usage: "userinfo <mention> or <id>"
-};
+}
 
 module.exports.help = {
     name: "userinfo"
-  }
+
+}
