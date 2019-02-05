fd62bf899ea391e1a5ca6a699cf8f08c4b8cdbae
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-05 19:14:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ bot.on('guildMemberAdd', member => {
   });
 
 bot.on("guildMemberRemove", async member => {
-    console.log(`${user.tag} left the server.`);
+    console.log(`${username} left the server.`);
 
     let welcomechannel = member.guild.channels.find(`name`, "left-members");
     welcomechannel.send(`${member} has left the server.`);
