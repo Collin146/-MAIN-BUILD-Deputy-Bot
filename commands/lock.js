28c13ce5fd2d0651d47496239fb944f9938f87e8
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-06 00:01:42 +0100

    Update lock.js

--- commands/lock.js
+++ commands/lock.js
@@ -1,6 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+    if(args[0] === "help"){
+        message.reply("Usage: !lock <channel>");
+        return;
+    }
     bot.on(message, [channel = message.channel]); {
     const type = channel.type === 'text' ? 'SEND_MESSAGES' : 'CONNECT';
     await channel.overwritePermissions(channel.guild.defaultRole, { [type]: false });
