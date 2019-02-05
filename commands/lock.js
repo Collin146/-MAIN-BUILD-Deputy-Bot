bef75b038828b75fba1aa3ea0d69f287384a8d9f
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-05 23:52:18 +0100

    Create lock.js

---
+++ commands/lock.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    async.run(msg, [channel = msg.channel]); {
+    const type = channel.type === 'text' ? 'SEND_MESSAGES' : 'CONNECT';
+    await channel.overwritePermissions(channel.guild.defaultRole, { [type]: false });
+    if (msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES') === false) return true;
+    return msg.send('This channel is locked.');
+  }
+}
+
+  module.exports.help = {
+    name: "lock"
+}
