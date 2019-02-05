0ba540afb61608c7dbf7649ab90d452635824ce6
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-05 01:24:57 +0100

    Create userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let bicon = displayAvatarURL;
+    let userembed = new Discord.RichEmbed()
+    .setDescription("User Information")
+    .setColor("#ff6a00")
+    .setThumbnail(bicon)
+    .addField("User Name", username)
+    .addField("User Tag", tag)
+    .addField("User ID", ID)
+    .addField("Account Created On", createdAt)
+    .addField("Joined Server On", joinedAt)
+    .addField("User Presence", presence);
+    
+    return message.channel.send(userembed);
+
+
+}
+
+module.exports.help = {
+    name: "userinfo"
+
+
+}
