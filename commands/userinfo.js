e811dab1deb96852f45f3e335af6cda726723b39
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-05 16:07:23 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,24 +1,17 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-
-if (message.content.startsWith(prefix + "userinfo"))
+if (msg.content.startsWith(prefix + "userinfo")) {
+    let member = msg.mentions.users.first() || msg.author;
     let userembed = new Discord.RichEmbed()
-        .setColor(message.guild.member(member).highestRole.color)
+        .setColor(msg.guild.member(member).highestRole.color)
         .setThumbnail(member.displayAvatarURL)
         
         .setTitle(`Here is ${member.username}'s info.`)
         .addField(`Name:`, member.username, true)
-        .addField(`ID:`, member.id, true)
+        .addField(`Id:`, member.id, true)
         .addField(`Bot:`, member.bot ? "Yes" : "No", true)
-        .addField("Game:", message.guild.member(member).presence.game ? message.guild.member(member).presence.game.name : "Not Playing", true) // the ? and : are like an if statement if (msg.guild.member(member).presence.game ) { msg.guild.member(member).presence.game.name } else "Not Playing"
-        .addField("Nickname:", message.guild.member(member).nickname ? message.guild.member(member).nickname : "None", true )
+        .addField("Game:", msg.guild.member(member).presence.game ? msg.guild.member(member).presence.game.name : "Not Playing", true) // the ? and : are like an if statement if (msg.guild.member(member).presence.game ) { msg.guild.member(member).presence.game.name } else "Not Playing"
+        .addField("Nickname:", msg.guild.member(member).nickname ? msg.guild.member(member).nickname : "None", true )
         .addField("Last Messsage:", member.lastMessage, true)
-        .addField(`Roles:`, message.guild.member(member).roles.map(s => s).join(" | "), true)
+        .addField(`Roles:`, msg.guild.member(member).roles.map(s => s).join(" | "), true)
 
-        message.channel.send(userembed);
-}
-
-module.exports.help = {
-    name: "userinfo"
+        msg.channel.send(userembed);
 }
