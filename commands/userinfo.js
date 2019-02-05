b839af2c38833a7a7c472714df7d15038971ef1c
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-05 01:52:44 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    const member = msg.mentions.members.first() || msg.guild.members.get(args[0]) || msg.member;
+    const member = msg.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
     const embed = new Discord.MessageEmbed()
     .setColor(randomColor)
     .setThumbnail(`${member.user.displayAvatarURL()}`)
@@ -11,11 +11,11 @@ module.exports.run = async (bot, message, args) => {
     .addField("Guild", `${bot}`, true)
     .addField("Status", `${status[member.user.presence.status]}`, true)
     .addField("Playing", `${member.user.presence.game ? `${member.user.presence.game.name}` : "not playing anything."}`, true)
-    .addField("Roles", `${member.roles.filter(r => r.id !== msg.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
+    .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
     .addField("Joined At", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
     .addField("Created At", `${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true);
 
-  msg.channel.send({embed});
+  message.channel.send({embed});
 
 }
 
