6e0c6028ff3088474511aa416a917d9268dc954e
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-06 18:45:14 +0100

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -3,6 +3,19 @@ const ms = require("ms");
 const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
+    
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+    if(args[0] === "help"){
+        message.reply("Usage: !lockdown <length>");
+        return;
+    }
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+    if(args[0] == "help"){
+        message.reply("Usage: !lockdown <length>");
+        return;
+    }
+    
 
     if (message.member.hasPermission("ADMINISTRATOR")) {
     if (!client.lockit) client.lockit = [];
