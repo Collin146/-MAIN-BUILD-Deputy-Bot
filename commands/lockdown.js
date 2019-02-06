72884814ee7464d1509bf49a7f6b3bcf157ee429
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-06 18:46:12 +0100

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -18,7 +18,6 @@ module.exports.run = async (bot, message, args) => {
     
 
     if (message.member.hasPermission("ADMINISTRATOR")) {
-    if (!client.lockit) client.lockit = [];
     let time = args.join(' ');
     let validUnlocks = ['release', 'unlock'];
     if (!time) return message.reply('You must set a duration for the lockdown in either hours, minutes or seconds');
