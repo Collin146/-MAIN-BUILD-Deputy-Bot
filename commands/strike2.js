752961e57ac65cc6a958242c2857c9c03dcf17ce
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-07 20:03:06 +0100

    Update strike2.js

--- commands/strike2.js
+++ commands/strike2.js
@@ -16,6 +16,7 @@ module.exports.run = async (bot, message, args) => {
         message.reply("Usage: !strike2 <user> <time length> <reason>");
         return;
     }
+    //te
 
 let tostrike = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 if(!tostrike) return message.reply("Couldn't find that user.");
