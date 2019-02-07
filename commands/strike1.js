587040696488ee251cb64b73885026e3246fb5b6
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-07 16:57:04 +0100

    Update strike1.js

--- commands/strike1.js
+++ commands/strike1.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const ms = require("ms");
+let strikes = JSON.parse(fs.readFileSync("./strikes.json", "utf8"));
 const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
