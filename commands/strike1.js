d14032478f2845fb1fe7e7a95641f6d4dee6ed9c
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-07 15:35:24 +0100

    Update strike1.js

--- commands/strike1.js
+++ commands/strike1.js
@@ -6,24 +6,24 @@ module.exports.run = async (bot, message, args) => {
 
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
     if(args[0] === "help"){
-        message.reply("Usage: !strike <user> <time>");
+        message.reply("Usage: !tempmute <user> <time>");
         return;
     }
     
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
     if(args[0] == "help"){
-        message.reply("Usage: !strike <user> <time length>");
+        message.reply("Usage: !tempmute <user> <time length>");
         return;
     }
 
 //!tempmute @user 1s/m/h/d
 
-let strike1 = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-if(!strike1) return message.reply("Couldn't find that user.");
+let tostrike = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!tostrike) return message.reply("Couldn't find that user.");
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
-if(strike1.hasPermission("MANAGE_MESSAGES")) return message.reply("You cannot mute a Moderator or higher");
+if(tostrike.hasPermission("MANAGE_MESSAGES")) return message.reply("You cannot mute a Moderator or higher");
 let strikerole = message.guild.roles.find(`name`, "Strike 1");
-let reason = args.join(" ").slice(22);
+//start of create role
 if (!strikerole){
     try{
         strikerole = await message.guild.createRole({
@@ -40,16 +40,16 @@ if (!strikerole){
         console.log(e.stack);
     }
 }
-
+//end of create role
 let striketime = args[1];
-if(!striketime) return message.reply("You didn't specify a time!");
+if(!striketiime) return message.reply("You didn't specify a time!");
 
-await(strike1.addRole(strike1.id));
-message.reply(`<@${strike1.id}> has been striked for ${ms(ms(striketime))}`);
+await(tostrike.addRole(strikerole.id));
+message.reply(`<@${tostrike.id}> has been muted for ${ms(ms(striketime))}`);
 
 setTimeout(function(){
-    strike1.removeRole(strikerole.id);
-    message.channel.send(`<@${strike1}.id}> has been removed from Strike 1!`);
+    tostrike.removeRole(strikerole.id);
+    message.channel.send(`<@${tostrike.id}> has been removed from Strike 1!`);
 }, ms(striketime));
 
 
