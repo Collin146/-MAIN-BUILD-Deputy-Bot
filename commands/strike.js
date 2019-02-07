a406050a6c66f7eb64662f8e9e411a5d56b637d6
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-07 10:35:24 +0100

    Create strike.js

---
+++ commands/strike.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+let role = message.guild.roles.find(r => r.name === "Team Mystic");
+
+// Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
+let member = message.mentions.members.first();
+
+// or the person who made the command: let member = message.member;
+
+// Add the role!
+member.addRole(role).catch(console.error);
+
+// Remove a role!
+member.removeRole(role).catch(console.error);
+}
+
+
+module.exports.help = {
+    name: "strike"
+
+}
