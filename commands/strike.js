1f0519ff49ca5a50964003cb9c14af39b531e1bf
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-07 10:44:28 +0100

    Update strike.js

--- commands/strike.js
+++ commands/strike.js
@@ -3,6 +3,19 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+    if(args[0] === "help"){
+        message.reply("Usage: !<Strike 1 or 2> <user> <reason>");
+        return;
+    }
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+    if(args[0] == "help"){
+        message.reply("Usage: !<Strike 1 or 2> <user> <reason>");
+        return;
+    }
+    
+
 let role = message.guild.roles.find(r => r.name === "Strike 1");
 
 // Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
