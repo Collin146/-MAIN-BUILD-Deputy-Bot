ef7a2df50274a058cb7642046385fde5b25819a6
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-07 10:46:22 +0100

    Update strike.js

--- commands/strike.js
+++ commands/strike.js
@@ -1,39 +1,39 @@
-const Discord = require("discord.js");
-const errors = require("../utils/errors.js");
+// const Discord = require("discord.js");
+// const errors = require("../utils/errors.js");
 
-module.exports.run = async (bot, message, args) => { 
+// module.exports.run = async (bot, message, args) => { 
 
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
-    if(args[0] === "help"){
-        message.reply("Usage: !<Strike 1 or 2> <user> <reason>");
-        return;
-    }
+//     if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+//     if(args[0] === "help"){
+//         message.reply("Usage: !<Strike 1 or 2> <user> <reason>");
+//         return;
+//     }
 
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
-    if(args[0] == "help"){
-        message.reply("Usage: !<Strike 1 or 2> <user> <reason>");
-        return;
-    }
+//     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+//     if(args[0] == "help"){
+//         message.reply("Usage: !<Strike 1 or 2> <user> <reason>");
+//         return;
+//     }
     
 
-let role = message.guild.roles.find(r => r.name === "Strike 1");
+// let role = message.guild.roles.find(r => r.name === "Strike 1");
 
-// Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
-let member = message.mentions.members.first();
+// // Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
+// let member = message.mentions.members.first();
 
-// or the person who made the command: let member = message.member;
+// // or the person who made the command: let member = message.member;
 
-// Add the role!
-message.member.addRole(role)
-  .then(console.log)
-  .catch(console.error);
+// // Add the role!
+// message.member.addRole(role)
+//   .then(console.log)
+//   .catch(console.error);
 
-// Remove a role!
-message.member.removeRole(role).catch(console.error);
-}
+// // Remove a role!
+// message.member.removeRole(role).catch(console.error);
+// }
 
 
-module.exports.help = {
-    name: "strike"
+// module.exports.help = {
+//     name: "strike"
 
-}
+// }
