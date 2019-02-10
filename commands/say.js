7d3a357b56bad7e91856c45a1c07af45a8ba0a38
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-11 00:06:01 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,12 +2,6 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if(message.content.startsWith(prefix)){
-        let commandfile = bot.commands.get(cmd.slice(prefix.length));
-        if(commandfile) commandfile.run(bot,message,args);
-        }else{
-        //you can put whatever you'd like here. like a coin system or something.
-        
 //!say hi
 //hi
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have permission to do that.");
@@ -15,7 +9,7 @@ let botmessage = args.join(" ");
 message.delete().catch();
 message.channel.send(botmessage);
 }
-}
+
 module.exports.help = {
     name: "say"
 }
