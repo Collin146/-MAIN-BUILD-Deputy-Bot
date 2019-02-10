89cfd240cef87c68e91703d33051515ed407554b
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-10 19:21:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
-   let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+   let prefixes = "!"
 
      if(!prefixes[message.guild.id]){
       prefixes[message.guild.id] = {
