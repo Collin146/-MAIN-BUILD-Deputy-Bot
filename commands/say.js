acc2f7af8672f1165fe27eb06725d0ed6608e942
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-10 17:43:32 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,16 +1,4 @@
 const Discord = require("discord.js");
-const botconfig = require("./Botconfig.json");
-
-bot.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd =  messageArray[0];
-    let args = messageArray.slice(1);
-
-});
 
 module.exports.run = async (bot, message, args) => {
 //!say hi
