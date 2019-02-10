4b70e4b02399f29231d14a6fa4297a56973c9964
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-10 23:22:42 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    var prefix = settings.prefix;
+    const args = message.content.split(" ");
+    const command = args.shift().slice(settings.prefix.length);
 //!say hi
 //hi
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have permission to do that.");
