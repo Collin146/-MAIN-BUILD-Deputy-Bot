42461667489c6aefa28bc71b34c40593fc6b5efc
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-12 14:03:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -82,5 +82,35 @@ bot.on("message", async message => {
 
 });
 
+//----
+//Deleted Messages Log Start
+//----
+
+client.on('messageDelete', async (message) => {
+    const logs = message.guild.channels.find(channel => channel.name === "logs");
+    if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
+      message.guild.createChannel('logs', 'deleted-messages-log');
+    }
+    if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) { 
+      console.log('The logs channel does not exist and tried to create the channel but I am lacking permissions')
+    }  
+    const entry = await message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE'}).then(audit => audit.entries.first())
+    let user = ""
+      if (entry.extra.channel.id === message.channel.id
+        && (entry.target.id === message.author.id)
+        && (entry.createdTimestamp > (Date.now() - 5000))
+        && (entry.extra.count >= 1)) {
+      user = entry.executor.username
+    } else { 
+      user = message.author.username
+    }
+    logs.send(`A message was deleted in ${message.channel.name} by ${user}`);
+  })
+
+  //----
+  //Deleted Messages Log End
+  //----
+
+
 
 bot.login(botconfig.token);
