815731e063fff155acbbb9685260c16419b096a9
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-02-23 01:43:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
+const antispam = require("discord-anti-spam");
 
 fs.readdir("./commands/", (err, files) => {
 
