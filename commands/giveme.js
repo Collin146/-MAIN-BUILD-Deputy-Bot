79081d43c3e6f61de61d8f3227dfa0005f6f22df
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-06 17:23:42 +0100

    Update giveme.js

--- commands/giveme.js
+++ commands/giveme.js
@@ -13,22 +13,48 @@ module.exports.run = async (bot, message, args) => {
     if(args[0] == "help"){
         message.reply("Usage: !bcso <user>");
         return;
-    }    
-    
+    }
+
 let tobcso= message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
 if(!tobcso) return message.reply("Couldn't find that user.");
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
 let memberrole = message.guild.roles.find(`name`, "Owner");
-let bcsorole = message.guild.roles.find(`name`, "BCSO");
-let probrole = message.guild.roles.find(`name`, "BCSO Probationary");
-let leorole = message.guild.roles.find(`name`, "LEO");
-let nmrole = message.guild.roles.find(`name`, "New Member");
+if(tobcso.hasPermission("ADMINISTRATOR")) return message.channel.send("You cannot use this command on an admin!");
 //let memrrole = message.guild.roles.find(`name`, "ember");
 //let approle = message.guild.roles.find(`name`, "Applicant");
 //guildmember.setRoles(...)
 
-tobcso.removeRole(memberrole.id);
+tobcso.addRole(memberrole.id);
 
+
+let ModEmbed = new Discord.RichEmbed()
+.setTitle("BCSO command used!")
+.setColor("GREEN")
+.addField("User", `<@${tobcso.id}>`, true)
+.addField("Command Used In", message.channel, true)
+.addField("Command Used By", message.author.username, true)
+.setTimestamp()
+.setFooter(`Message ID: ${message.id} | Author ID: ${message.author.id}`);
+
+let warnchannel = message.guild.channels.find(`name`, "modlog");
+if(!warnchannel) return message.reply("Couldn't find channel");
+    
+if(message.content === "1779_check") {
+    const ayy = client.emojis.find(emoji => emoji.name === "1779_check");
+ }
+
+warnchannel.send(ModEmbed);
+
+geluktEmbed = new Discord.RichEmbed()
+      .setAuthor(`The roles have successfully been updated!`)
+      .setColor("GREEN")
+      .setAuthor("${ayy} Done!")
+      .setDescription(`The roles of <@${tobcso.id}> have been updated!`)
+      .setFooter(`Mentioned User ID: ${tobcso.id}`);
+
+      message.channel.send(geluktEmbed);
+
+//end of module
 
 }
 
