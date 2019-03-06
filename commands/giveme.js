64dc59cfceebb4e4d93aee66ce109497bb897dbb
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-06 17:20:44 +0100

    Update giveme.js

--- commands/giveme.js
+++ commands/giveme.js
@@ -3,9 +3,22 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
-    let tobcso= message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "ADMINISTRATOR");
+    if(args[0] === "help"){
+        message.reply("Usage: !bcso <user>");
+        return;
+    }
+    
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You don't have permission to do that.");
+    if(args[0] == "help"){
+        message.reply("Usage: !bcso <user>");
+        return;
+    }    
+    
+let tobcso= message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
 if(!tobcso) return message.reply("Couldn't find that user.");
-let memberrole = message.guild.roles.find(`name`, "Staff");
+let memberrole = message.guild.roles.find(`name`, "Owner");
 let bcsorole = message.guild.roles.find(`name`, "BCSO");
 let probrole = message.guild.roles.find(`name`, "BCSO Probationary");
 let leorole = message.guild.roles.find(`name`, "LEO");
