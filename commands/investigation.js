8bd640a9c77073f68b9ea2f002d12917be5d2a7c
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-06 10:03:38 +0100

    Update investigation.js

--- commands/investigation.js
+++ commands/investigation.js
@@ -23,7 +23,7 @@ let investrole = message.guild.roles.find(`name`, "Investigation");
 //let approle = message.guild.roles.find(`name`, "Applicant");
 //guildmember.setRoles(...)
 
-toinvest.removeRoles;
+toinvest.removeRoles();
 toinvest.addRole(investrole.id);
 
 let ModEmbed = new Discord.RichEmbed()
