823ce5fb3e6e792e0c2c15dfdeedd032e4341855
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-06 11:55:14 +0100

    Create sahp.js

---
+++ commands/sahp.js
@@ -0,0 +1,57 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+    if(!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "ADMINISTRATOR");
+    if(args[0] === "help"){
+        message.reply("Usage: !sahp <user>");
+        return;
+    }
+    
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You don't have permission to do that.");
+    if(args[0] == "help"){
+        message.reply("Usage: !sahp <user>");
+        return;
+    }
+
+let tosahp = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!tosahp) return message.reply("Couldn't find that user.");
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+let memberrole = message.guild.roles.find(`name`, "Member");
+let sahprole = message.guild.roles.find(`name`, "SAHP");
+let probrole = message.guild.roles.find(`name`, "SAHP Probationary");
+let leorole = message.guild.roles.find(`name`, "LEO");
+let nmrole = message.guild.roles.find(`name`, "New Member");
+if(tosahp.hasPermission("ADMINISTRATOR")) return message.channel.send("You cannot use this command on an admin!");
+//let memrrole = message.guild.roles.find(`name`, "ember");
+//let approle = message.guild.roles.find(`name`, "Applicant");
+//guildmember.setRoles(...)
+
+tosahp.removeRole(nmrole.id);
+tosahp.addRole(memberrole.id);
+tosahp.addRole(sahprole.id);
+tosahp.addRole(probrole.id);
+tosahp.addRole(leorole.id);
+
+let ModEmbed = new Discord.RichEmbed()
+.setTitle("SAHP command used!")
+.setColor("GREEN")
+.addField("User", `<@${tosahp.id}>`, true)
+.addField("Command Used In", message.channel, true)
+.addField("Command Used By", message.author.username, true)
+.setTimestamp()
+.setFooter(`Message ID: ${message.id} | Author ID: ${message.author.id}`);
+
+let warnchannel = message.guild.channels.find(`name`, "modlog");
+if(!warnchannel) return message.reply("Couldn't find channel");
+    
+warnchannel.send(ModEmbed);
+
+//end of module
+
+}
+
+module.exports.help = {
+    name: "sahp"
+}
