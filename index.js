bfbaf520b31cf4264f57fc2870fee89a2ec63f1e
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-08 11:57:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ bot.on("guildMemberRemove", async member => {
 
 bot.on("ready", async () => {
  console.log(`${bot.user.username} is online!`);
- bot.user.setActivity("!help | prefix !");
+ bot.user.setActivity("v501 | !help | prefix !");
 
 });
 
