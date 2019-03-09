0b3f4e2a0d0546fc391840a052835915aee043cf
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-10 00:57:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ bot.on("guildMemberRemove", async member => {
 
 bot.on("ready", async () => {
  console.log(`${bot.user.username} is online!`);
- bot.user.setActivity("v516.5 | !help | prefix !");
+ bot.user.setActivity("Undergoing Maintenance!");
 
 });
 
