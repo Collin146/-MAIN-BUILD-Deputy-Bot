37978070f25ddc57347c10027c205a57d0d26eb2
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-10 10:51:25 +0100

    Update session.js

--- commands/session.js
+++ commands/session.js
@@ -22,7 +22,7 @@ message.channel.send([
     `<@&${mentionrole.id}>`,
     ` `,
     "**Session on**",
-    `__${day}__ **at** __${time}__ **PM GMT**`,
+    `\`${day}\` **at** \`${time}\` **PM GMT**`,
     ` `,
     "**Say yes to attend**",
     "(if you say yes or maybe and dont show up without a valid reason, you will receive a strike)"
