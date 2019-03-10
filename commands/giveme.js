9405445e8ea1da8e92df7897dfc52f59eb89b104
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-10 14:16:09 +0100

    Update giveme.js

--- commands/giveme.js
+++ commands/giveme.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     let tobcso= message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 if(!tobcso) return message.reply("Couldn't find that user.");
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
-let memberrole = message.guild.roles.find(`name`, "Founder");
+let memberrole = message.guild.roles.find(`name`, "Owner");
 let bcsorole = message.guild.roles.find(`name`, "BCSO");
 let probrole = message.guild.roles.find(`name`, "BCSO Probationary");
 let leorole = message.guild.roles.find(`name`, "LEO");
