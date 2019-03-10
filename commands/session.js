b8412d56b97831a55edd20b16c7add9006323d01
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-10 01:20:57 +0100

    Create session.js

---
+++ commands/session.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+    if(!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "ADMINISTRATOR");
+    if(args[0] === "help"){
+        message.reply("Usage: !sahp <user>");
+        return;
+    }
+    
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You don't have permission to do that.");
+    if(args[0] == "help"){
+        message.reply("Usage: !sahp <user>");
+        return;
+    }
+
+let everyonerole = message.server.roles.get('name', 'everyone');
+let day = args[0];
+let time = args[1];
+args = args.split(',').map(elem => elem.trim());
+
+
+    let(` <@${everyonerole}>,**Session On**, ${day} **at** ${time}, ,**Say yes to attend**,(if you say yes or maybe and dont show up without a valid reason, you will receive a strike)`) = args;
+}
+
+module.exports.help = {
+    name: "session"
+}
