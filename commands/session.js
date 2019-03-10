9a5358110389e9ce5c65760d7aaf07747cf705a5
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-10 02:53:10 +0100

    Update session.js

--- commands/session.js
+++ commands/session.js
@@ -19,7 +19,7 @@ let day = args[0];
 let time = args[1];
 
 message.channel.send([
-    `<@&448153555514163200>`,
+    `<&487025249884569600>`,
     ` `,
     "**Session on**",
      `${day} **at** ${time} **PM GMT**`,
