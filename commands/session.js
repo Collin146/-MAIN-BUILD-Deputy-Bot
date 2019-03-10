99c91456dac410396521f933df5f3a0fd998510d
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-10 01:30:17 +0100

    Update session.js

--- commands/session.js
+++ commands/session.js
@@ -15,11 +15,11 @@ module.exports.run = async (bot, message, args) => {
         return;
     }
 
-let args = command.substr( command.indexOf(' ') + 1 );
+let args2 = command.substr( command.indexOf(' ') + 1 );
 let everyonerole = message.guild.roles.find('name', 'Member');
 let day = args[0];
 let time = args[1];
-args = args.split(',').map(elem => elem.trim());
+args2 = args.split(',').map(elem => elem.trim());
 
 
     let(` <@${everyonerole}>,**Session On**, ${day} **at** ${time}, ,**Say yes to attend**,(if you say yes or maybe and dont show up without a valid reason, you will receive a strike)`) = args;
