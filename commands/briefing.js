e8830b4abbbacea3a5ca171867ca3650905278eb
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-10 15:31:50 +0100

    Create briefing.js

---
+++ commands/briefing.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+    if(args[0] === "help"){
+        message.reply("Usage: !tempmute <user> <time>");
+        return;
+    }
+    
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+    if(args[0] == "help"){
+        message.reply("Usage: !tempmute <user> <time length>");
+        return;
+    }
+
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+let mentionrole = message.guild.roles.find(`name`, `Member`);
+
+setTimeout(function(){
+    message.channel.send([
+        `<@&${mentionrole.id}>`,
+        ` `,
+        "⚠ **Join Briefing Room Now!** ⚠"
+      ].join('\n'))
+}, ms(mutetime));
+
+}
+
+module.exports.help = {
+    name: "briefing"
+}
