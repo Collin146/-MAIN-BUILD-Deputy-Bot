40f013e28addb3bcdb68a2b63085cd4395d5e3dd
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-11 20:44:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -113,6 +113,7 @@ bot.on(`message`, async message => {
         if (bannedWords.some(word => message.content.toLowerCase().includes(word))) {
             if (message.author.id === message.guild.ownerID) return;
             if (message.member.hasPermission("ADMINISTRATOR")) return;
+            if (message.member.roles.find("name", "Content Creator")) return;
             await message.delete();
             
             let linkembed = new Discord.RichEmbed()
