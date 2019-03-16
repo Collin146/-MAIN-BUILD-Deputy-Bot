c123cda14143bf63a58e3c5c004e7670b548d4b3
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-16 23:15:24 +0100

    Update intimidation.js

--- charges/intimidation.js
+++ charges/intimidation.js
@@ -4,7 +4,7 @@ const errors = require("../utils/errors.js");
 module.exports.run = async (bot, message, args) => { 
 
 let intimembed = new Discord.RichEmbed()
-.setTitle("**Intimidation(1)01 Info**")
+.setTitle("**Intimidation (1)01 Info**")
 .addField("Type of Punishment", "Misdemeanor")
 .addField("Punishment(s)", "2 Minutes in prison.")
 .addField("Information", "Intimidation differs from assault in terms of the distance between the acts. Intimidation can occur across the street, while assault is up close, within reach of leading to battery. Intimidation replaces criminal threats.");
