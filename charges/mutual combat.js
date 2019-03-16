7d14063630c5e90aa8057c269cfa3eb98af11878
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-17 00:00:07 +0100

    Update mutual combat.js

--- charges/mutual combat.js
+++ charges/mutual combat.js
@@ -3,6 +3,8 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+if(args[0] === "combat"){
+
 let intimembed = new Discord.RichEmbed()
 .setTitle("**Mutual Combat (1)04 Info**")
 .setColor("BLACK")
@@ -11,6 +13,8 @@ let intimembed = new Discord.RichEmbed()
 .addField("Information", "A person who engages in mutual combat with another individual in an area accessible to the public, or in public view, regardless of the consent of the individuals involved.");
 
 message.channel.send(intimembed);
+
+}
 
 }
 
