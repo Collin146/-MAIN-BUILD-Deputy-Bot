e03ed505127460de23b9c39a96999ce339610aaa
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-16 22:45:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
-fs.readdir("./commands/", "../charges/", (err, files) => {
+fs.readdir("./commands/", (err, files) => {
 
 if(err) console.log(err);
 
@@ -18,10 +18,8 @@ if(jsfile.length <= 0){
 
 jsfile.forEach((f, i) =>{
     let props = require(`./commands/${f}`);
-    let props2 = require(`../charges/${f}`);
     console.log(`${f} loaded!`);
     bot.commands.set(props.help.name, props);
-    bot.commands.set(props2.help.name, props2);
 
 });
 
