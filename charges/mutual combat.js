926378a6da96f596bc3e15fcc4590eb153427132
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-16 23:57:56 +0100

    Create mutual combat.js

---
+++ charges/mutual combat.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+let intimembed = new Discord.RichEmbed()
+.setTitle("**Mutual Combat (1)04 Info**")
+.setColor("BLACK")
+.addField("Type of Punishment", "Misdemeanor")
+.addField("Punishment(s)", "3 Minutes in prison.")
+.addField("Information", "A person who engages in mutual combat with another individual in an area accessible to the public, or in public view, regardless of the consent of the individuals involved.");
+
+message.channel.send(intimembed);
+
+}
+
+module.exports.help = {
+    name: "mutual combat"
+}
