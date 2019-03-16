6b5f3ab6cdf9ab1fae7a0dd2a4e4a6e64cad7e0d
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-16 22:28:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,17 +22,30 @@ jsfile.forEach((f, i) =>{
     bot.commands.set(props.help.name, props);
 
 });
+
+});
+
+fs.readdir("../charges/", (err, files) => {
+
+    if(err) console.log(err);
     
-        jsfile.forEach((f, i) =>{
-        let props = require(`./charges/${f}`);
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0){
+        console.log("Couldn't find commands.");
+        return;
+    
+    
+    }
+    
+    jsfile.forEach((f, i) =>{
+        let props = require(`../charges/${f}`);
         console.log(`${f} loaded!`);
         bot.commands.set(props.help.name, props);
     
+    });
+    
     
     });
-
-
-});
 
 bot.on('guildMemberAdd', member => {
     let welcomechannel = member.guild.channels.find(`name`, "welcome");
