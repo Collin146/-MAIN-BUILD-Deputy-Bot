9896ce7149c329e547bad8adc0f313f71f711c8b
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-16 23:32:56 +0100

    Create assualt.js

---
+++ charges/assualt.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+let intimembed = new Discord.RichEmbed()
+.setTitle("**Assault (1)02 Info**")
+.setColor("BLACK")
+.addField("Type of Punishment", "Misdemeanor")
+.addField("Punishment(s)", "3 Minutes in prison.")
+.addField("Information", "Assault is defined by distance the threats occur. Someone a few feet away threatening to harm or kill you at any moment is assault. Intimidation is less severe as there’s a distance that someone can escape through, or that the perpetrator has more time to reconsider the threat. Any violent physical contact is considered to be battery, however grabbing someone during a threat may be either assault or battery, depending on intention and interpretation.");
+
+message.channel.send(intimembed);
+
+}
+
+module.exports.help = {
+    name: "assault"
+}
