afc6f225952bc96eb7fcd351a02e6db97e70bbb7
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-17 23:42:55 +0100

    Create statutory.js

---
+++ charges/statutory.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+  if(args[0] === "rape"){
+
+let argembed = new Discord.RichEmbed()
+  .setTitle("**Statutory Rape (3)09 Info**")
+  .setColor("BLACK")
+  .addField("Type of Punishment", "Felony")
+  .addField("Punishment(s)", "6 Minutes in prison")
+  .addField("Information", "A person who engages in mutually-interested sexual intercourse with another who is under the age of 18 and therefore cannot give legal consent.");
+
+  message.channel.send(argembed);
+
+  return; 
+  }
+
+  message.channel.reply("If you are looking for \'Statutory Rape\' type: !statutory rape")
+
+  
+
+}
+
+module.exports.help = {
+    name: "statutory"
+}
