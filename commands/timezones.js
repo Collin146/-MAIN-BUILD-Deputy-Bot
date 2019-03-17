e867050deb8327116154c4d4c76b580471fd30da
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-17 14:12:48 +0100

    Update timezones.js

--- commands/timezones.js
+++ commands/timezones.js
@@ -3,35 +3,107 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
-    if(!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "ADMINISTRATOR");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
     if(args[0] === "help"){
         message.reply("Usage: !sahp <user>");
         return;
     }
     
-    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You don't have permission to do that.");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
     if(args[0] == "help"){
         message.reply("Usage: !sahp <user>");
         return;
     }
 
-    let pdttime = (args[0] - 7);
-    let edttime = (args[0] - 4);
-    let cettime = (args[0] + 1);
-    let mdttime = (args[0] - 6);
-    let cdttime = (args[0] - 5);
-    let arg2 = args[1];
+    if(args[0] === "6:30"){
     
-    message.channel.send([
-        "**Other Timezones:**",
-        `PDT: ${pdttime}${arg2}`,
-        `EDT: ${edttime}${arg2}`,
-        `CET: ${cettime}${arg2}`,
-        `MDT: ${mdttime}${arg2}`,
-        `CDT: ${cdttime}${arg2}`,
-      ].join('\n'))
-    
+        //let time = args[0];
+        
+message.channel.send([
+    "**Other Timezones:**",
+    "PDT: 11:30 AM",
+    "EDT: 2:30 PM",
+    "CET: 2:30 PM",
+    "MDT: 12:30 PM",
+    "CDT: 1:30 PM"
+   ].join('\n'))
+        
+   return;
+    }
 
+    if(args[0] === "7:00"){
+
+message.channel.send([
+    "**Other Timezones:**",
+    "PDT: 12:00 PM",
+    "EDT: 3:00 PM",
+    "CET: 8:00 PM",
+    "MDT: 1:00 PM",
+    "CDT: 2:00 PM"
+   ].join('\n'))
+                
+   return;
+    }
+
+
+if(args[0] === "7:30"){
+    
+//let time = args[0];
+
+message.channel.send([
+    "**Other Timezones:**",
+    "PDT: 12:30 PM",
+    "EDT: 3:30 PM",
+    "CET: 8:30 PM",
+    "MDT: 1:30 PM",
+    "CDT: 2:30 PM"
+   ].join('\n'))
+
+  return;
+    }
+
+    if(args[0] === "8:00"){
+
+message.channel.send([
+    "**Other Timezones:**",
+    "PDT: 1:00 PM",
+    "EDT: 4:00 PM",
+    "CET: 9:00 PM",
+    "MDT: 2:00 PM",
+    "CDT: 3:00 PM"
+   ].join('\n'))
+        
+   return;
+    }
+
+    if(args[0] === "8:30"){
+
+message.channel.send([
+    "**Other Timezones:**",
+    "PDT: 1:30 PM",
+    "EDT: 4:30 PM",
+    "CET: 9:30 PM",
+    "MDT: 2:30 PM",
+    "CDT: 3:30 PM"
+   ].join('\n'))
+                
+   return;
+    }
+
+     if(args[0] === "9:00"){
+
+ message.channel.send([
+    "**Other Timezones:**",
+    "PDT: 2:00 PM",
+    "EDT: 5:00 PM",
+    "CET: 10:00 PM",
+    "MDT: 3:00 PM",
+    "CDT: 4:00 PM"
+   ].join('\n'))
+                        
+   return;
+    }
+    
 }
 
 
