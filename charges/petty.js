f87496bf6c47c631fcaa3557ed0b49fe46462584
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-17 17:34:06 +0100

    Create petty.js

---
+++ charges/petty.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+  if(args[0] === "theft"){
+
+let argembed = new Discord.RichEmbed()
+  .setTitle("Petty Theft (2)08 Info**")
+  .setColor("BLACK")
+  .addField("Type of Punishment", "Misdemeanor")
+  .addField("Punishment(s)", "2 Minutes in prison")
+  .addField("Information", "A person who steals or takes the personal property of another worth $2,500 or less.");
+
+  message.channel.send(argembed);
+
+  return; 
+  }
+
+  message.channel.reply("If you are looking for \'Petty Theft\' type: !petty theft")
+
+  
+
+}
+
+module.exports.help = {
+    name: "petty"
+}
