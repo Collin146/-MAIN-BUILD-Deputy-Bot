d7e96c3817af02749d97c5f951e83545409a242c
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-17 17:57:28 +0100

    Update grand.js

--- charges/grand.js
+++ charges/grand.js
@@ -3,19 +3,20 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
-if(args[0] === "theft"){
+    if(args[3] === "firearm"){
 
-let intimembed = new Discord.RichEmbed()
-.setTitle("**Grand Theft (2)10 Info**")
-.setColor("BLACK")
-.addField("Type of Punishment", "Felony")
-.addField("Punishment(s)", "4 Minutes in prison.")
-.addField("Information", "A person who steals or takes the personal property of another worth $10,000 or more.");
-
-message.channel.send(intimembed);
-
-    return;
-}
+        let intimembed3 = new Discord.RichEmbed()
+        .setTitle("**Grand Theft Of A Firearm (2)12 Info**")
+        .setColor("BLACK")
+        .addField("Type of Punishment", "Felony")
+        .addField("Punishment(s)", "5 Minutes in prison.")
+        .addField("Information", "Grand Theft Of A Firearm does stack with Theft.");
+    
+        message.channel.send(intimembed3);
+    
+        return;
+    }
+    
 
 if(args[1] === "auto"){
 
@@ -31,19 +32,21 @@ if(args[1] === "auto"){
     return;
 }
 
-if(args[3] === "firearm"){
+if(args[0] === "theft"){
 
-    let intimembed3 = new Discord.RichEmbed()
-    .setTitle("**Grand Theft Of A Firearm (2)12 Info**")
+    let intimembed = new Discord.RichEmbed()
+    .setTitle("**Grand Theft (2)10 Info**")
     .setColor("BLACK")
     .addField("Type of Punishment", "Felony")
-    .addField("Punishment(s)", "5 Minutes in prison.")
-    .addField("Information", "Grand Theft Of A Firearm does stack with Theft.");
-
-    message.channel.send(intimembed3);
-
+    .addField("Punishment(s)", "4 Minutes in prison.")
+    .addField("Information", "A person who steals or takes the personal property of another worth $10,000 or more.");
+    
+    message.channel.send(intimembed);
+    
     return;
+   
 }
+    
 message.reply("If you are looking for \'Grand Theft\' type: !grand theft. If you are looking for \'Grand Theft Auto\' type: !grand theft auto. If you are looking for \'Grand Theft Of A Firearm\' type: !grand theft of a firearm.")
 
 }
