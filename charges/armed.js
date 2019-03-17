8bcd07b385606c1e983de12473cd3c6cf9eaa0d2
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-17 18:07:54 +0100

    Update armed.js

--- charges/armed.js
+++ charges/armed.js
@@ -17,7 +17,7 @@ let argembed = new Discord.RichEmbed()
   return; 
   }
 
-  message.channel.reply("If you are looking for \'Armed Robbery\' type: !armed robbery")
+  message.reply("If you are looking for \'Armed Robbery\' type: !armed robbery");
 
   
 
