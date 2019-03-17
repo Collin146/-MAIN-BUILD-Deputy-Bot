f1bd5e702204f7afec6a2fd3978f1bbd30a03224
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-17 17:31:39 +0100

    Create armed.js

---
+++ charges/armed.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+  if(args[0] === "robbery"){
+
+let argembed = new Discord.RichEmbed()
+  .setTitle("Armed Robbery (2)07 Info**")
+  .setColor("BLACK")
+  .addField("Type of Punishment", "Felony")
+  .addField("Punishment(s)", "7 Minutes in prison")
+  .addField("Information", "A person who takes property from the possession of another against their will, by means of force facilitated with a weapon or with an item used as a weapon. This charge cannot stack with (2)06. Robbery.");
+
+  message.channel.send(argembed);
+
+  return; 
+  }
+
+  message.channel.reply("If you are looking for \'Armed Robbery\' type: !armed robbery")
+
+  
+
+}
+
+module.exports.help = {
+    name: "armed"
+}
