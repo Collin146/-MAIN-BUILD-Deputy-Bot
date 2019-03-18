061e3371d2edb6e3333038dafd83cff06513d204
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-18 16:54:41 +0100

    Create contempt.js

---
+++ charges/contempt.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+if(args[1] === "court"){
+
+let intimembed = new Discord.RichEmbed()
+.setTitle("**Contempt of Court (4)03 Info**")
+.setColor("BLACK")
+.addField("Type of Punishment", "Misdemeanor")
+.addField("Punishment(s)", "2 Minutes in prison.")
+.addField("Information", "A person who willfully disobeys the verbal or written order of a court authority, disrespects the decorum of the court, or otherwise infringes upon due process. This charge can only be issued by a Judge or agent of a court.");
+
+message.channel.send(intimembed);
+
+return;
+}
+
+message.reply("If you are looking for \'Contempt of Court\' type: !contempt of court.")
+
+}
+
+module.exports.help = {
+    name: "contemp"
+}
