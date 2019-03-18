57d62a3107f48f60cce201f90b6e1ac27199de52
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-18 17:25:47 +0100

    Create perjury.js

---
+++ charges/perjury.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+let intimembed = new Discord.RichEmbed()
+.setTitle("**Perjury (4)08 Info**")
+.setColor("BLACK")
+.addField("Type of Punishment", "Felony")
+.addField("Punishment(s)", "4 Minutes in prison.")
+.addField("Information", "A person who knowingly provides false information while under oath in a court of law. A person who knowingly provides false information as part of an affidavit, testimony, court-ordered deposition, or document with a statement signifying its authenticity under penalty of perjury. This charge cannot stack with (4)06. False Information To A Government Employ");
+
+message.channel.send(intimembed);
+
+}
+
+module.exports.help = {
+    name: "pergury"
+}
