599b093b4bf550b80af98f321f7c88e70f94d03c
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-18 17:34:01 +0100

    Update failure.js

--- charges/failure.js
+++ charges/failure.js
@@ -3,6 +3,20 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+    if(args[1] === "indentify"){
+
+        let intimembed = new Discord.RichEmbed()
+        .setTitle("**Failure To Identify To A Peace Officer (4)09 Info**")
+        .setColor("BLACK")
+        .addField("Type of Punishment", "Misdemeanor")
+        .addField("Punishment(s)", "3 Minutes in prison.")
+        .addField("Information", "As per (12)11. Maximum Imprisonment someone who fails to identify and provide a way to properly charge them will be imprisoned 900 minutes until they identify themselves, after which this charge, plus all applicable charges, can be placed on their record and their sentence adjusted.");
+        
+        message.channel.send(intimembed);
+        
+        return;
+        }
+
 if(args[1] === "pay"){
 
 let intimembed = new Discord.RichEmbed()
@@ -17,7 +31,7 @@ message.channel.send(intimembed);
 return;
 }
 
-message.reply("If you are looking for \'Failure To Pay A Fine\' type: !failure to pay a fine.")
+message.reply("If you are looking for \'Failure To Pay A Fine\' type: !failure to pay a fine. If you are looking for \'Failure To Identify To A Peace Officer\' type: !failure to identify to a peace officer.")
 
 }
 
