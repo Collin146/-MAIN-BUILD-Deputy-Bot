cc677de6a1c164aaa2edd6774c7f876766226151
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-18 20:40:50 +0100

    Create corruption.js

---
+++ charges/corruption.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+  if(args[2] === "duty"){
+
+let argembed = new Discord.RichEmbed()
+  .setTitle("**Corruption Of Public Duty (4)22 Info**")
+  .setColor("BLACK")
+  .addField("Type of Punishment", "Felony")
+  .addField("Punishment(s)", "6 Minutes in prison")
+  .addField("Information", "A government employee who acts outside the interests of the public good or public justice. A government employee who demonstrates criminal negligence in their duties. A government employee convicted by the Department of Justice for committing a felony while on duty.");
+
+  message.channel.send(argembed);
+
+  return; 
+  }
+
+message.reply("If you are looking for \'Corruption Of Public Duty\' type: !corruption of public duty");
+
+}
+
+module.exports.help = {
+    name: "corruption"
+}
