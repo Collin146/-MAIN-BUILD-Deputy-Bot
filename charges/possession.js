8dfd10ad56f11ef0fe4137b4749a102623bff8a2
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-19 17:17:16 +0100

    Update possession.js

--- charges/possession.js
+++ charges/possession.js
@@ -3,6 +3,48 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+  if(args[4] === "with"){
+
+    let arg3embed = new Discord.RichEmbed()
+      .setTitle("**Possession Of A Controlled Substance With Intent To Sell (6)02 Info**")
+      .setColor("BLACK")
+      .addField("Type of Punishment", "Felony")
+      .addField("Punishment(s)", "4 Minutes in prison")
+      .addField("Information", "A person in possession of a controlled substance or multiple controlled substances in an amount of over one ounce (28 grams).");
+    
+      message.channel.send(arg3embed);
+    
+      return; 
+      }
+
+      if(args[2] === "drug"){
+
+        let arg4embed = new Discord.RichEmbed()
+          .setTitle("**Possession Of Drug Paraphernalia (6)03 Info**")
+          .setColor("BLACK")
+          .addField("Type of Punishment", "Infraction")
+          .addField("Punishment(s)", "$780")
+          .addField("Information", "A person who willingly possesses a device or mechanism used exclusively for the processing or consumption of an illegal controlled substance.");
+        
+          message.channel.send(arg4embed);
+        
+          return; 
+          }
+
+  if(args[2] === "controlled"){
+
+    let arg2embed = new Discord.RichEmbed()
+      .setTitle("**Possession Of A Controlled Substance (6)01 Info**")
+      .setColor("BLACK")
+      .addField("Type of Punishment", "Misdemeanor")
+      .addField("Punishment(s)", "3 Minutes in prison")
+      .addField("Information", "A person who possesses any controlled substance, except when the substance has been lawfully prescribed to them by a licensed practitioner of medicine or is legally available without a prescription.");
+    
+      message.channel.send(arg2embed);
+    
+      return; 
+      }
+
   if(args[1] === "burglary"){
 
 let argembed = new Discord.RichEmbed()
