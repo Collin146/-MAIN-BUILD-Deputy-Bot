a04a07d2a71076cdf43b5d1377f2ac87e9c2b08d
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-19 16:37:16 +0100

    Update and rename terrorism,js to terrorism.js

--- charges/terrorism.js
+++ charges/terrorism.js
@@ -9,6 +9,7 @@ let intimembed = new Discord.RichEmbed()
 .addField("Type of Punishment", "Felony")
 .addField("Punishment(s)", "12 Minutes in prison.")
 .addField("Information", "A person who, uses systematic threats or actions against the public good to cause fear and intimidation at a grand scale. A person who commits an attack or threatens an attack on a major public or private facility, such as a office complex, stadium, public transportation system, bridge, or other such structure. This charge can only be issued at the order of the Governor or Chief Justice.");
+
 message.channel.send(intimembed);
 
 }
