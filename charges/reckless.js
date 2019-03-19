6d02fe16ca4a82a050178ca0b2633ff3577de6ee
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-19 23:51:20 +0100

    Update reckless.js

--- charges/reckless.js
+++ charges/reckless.js
@@ -3,6 +3,20 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+    if(args[3] === "off-road"){
+
+        let arg2embed = new Discord.RichEmbed()
+          .setTitle("**Reckless Operation Of An Off-Road Or Naval Vehicle (8)10 Info**")
+          .setColor("BLACK")
+          .addField("Type of Punishment", "Misdemeanor")
+          .addField("Punishment(s)", "6 Minutes in prison")
+          .addField("Information", "A person who demonstrates careless or general disregard for the safety of themselves or others while operating a naval vehicle or vehicle intended for off-road travel.");
+        
+          message.channel.send(arg2embed);
+        
+          return; 
+          }
+
   if(args[0] === "operation"){
 
 let argembed = new Discord.RichEmbed()
@@ -17,7 +31,7 @@ let argembed = new Discord.RichEmbed()
   return; 
   }
 
-  message.reply("If you are looking for \'Reckess Operation Of An Aircraft\' type: !reckless operation of an aircraft");
+  message.reply("If you are looking for \'Reckess Operation Of An Aircraft\' type: !reckless operation of an aircraft. If you are looking for \'Reckess Operation Of An Off-Road Or Naval Vehicle\' type: !reckless operation of an off-road or naval vehicle");
 
 }
 
