087bcc8a8272b2b40d1efe2b1e1fb074d24575c2
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-20 19:14:40 +0100

    Update illegal.js

--- charges/illegal.js
+++ charges/illegal.js
@@ -3,6 +3,20 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+    if(args[3] === "aircraft"){
+
+        let intim3embed = new Discord.RichEmbed()
+        .setTitle("**Illegal Parking Of An Aircraft (11)16 Info**")
+        .setColor("BLACK")
+        .addField("Type of Punishment", "Infraction")
+        .addField("Punishment(s)", "$230")
+        .addField("Information", "A person who fails to follow the flight protocols as detailed in Section 5 of the State Aviation Act Of 2015");
+        
+        message.channel.send(intim3embed);
+        
+        return;
+        }
+
 if(args[0] === "nitrous"){
 
 let intimembed = new Discord.RichEmbed()
@@ -19,19 +33,19 @@ return;
 
 if(args[0] === "usage"){
 
-    let intimembed = new Discord.RichEmbed()
+    let intim2embed = new Discord.RichEmbed()
     .setTitle("**Illegal Usage Of Hydraulics (11)08 Info**")
     .setColor("BLACK")
     .addField("Type of Punishment", "Infraction")
     .addField("Punishment(s)", "$850")
     .addField("Information", "A person driving a vehicle that uses hydraulic equipment while in motion or on a public street, road, or highway. This excludes vehicles with hydraulic equipment permitted exclusively for business and equipment purposes, such as a forklift in motion");
     
-    message.channel.send(intimembed);
+    message.channel.send(intim2embed);
     
     return;
     }
 
-message.reply("If you are looking for \'Illegal Nitrous Oxide Possession\' type: !illegal nitrous oxide possession. If you are looking for \'Illegal Usage Of Hydraulics\' type: !illegal usage of hydraulics")
+message.reply("If you are looking for \'Illegal Nitrous Oxide Possession\' type: !illegal nitrous oxide possession. If you are looking for \'Illegal Usage Of Hydraulics\' type: !illegal usage of hydraulics. If you are looking for \'Illegal Parking Of An Aircraft\' type: !illegal parking of an aircraft.")
 
 }
 
