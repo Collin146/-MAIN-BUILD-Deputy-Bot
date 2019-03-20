3819243d38407666a96b19579102e36f6b182871
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-20 14:43:58 +0100

    Update reject.js

--- commands/reject.js
+++ commands/reject.js
@@ -25,8 +25,8 @@ let rejectembed = new Discord.RichEmbed()
 .setTitle(`Your application for ${message.guild.name} has been rejected!`)
 .setColor("RED")
 .addField("Reason", kReason)
-.addField("What To Do", "You can re apply through filling out the application form. Make sure you improve on the things you did wrong in the first try. Otherwise it will be rejected again.")
-.setTimestamp();
+.setTimestamp()
+.addField("What To Do", "You can re apply through filling out the application form. Make sure you improve on the things you did wrong in the first try. Otherwise it will be rejected again.");
 
 rUser.send(rejectembed);
     
