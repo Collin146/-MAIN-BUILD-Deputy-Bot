0ee4949e5d4458dc3f13fac2e61fe3698873f895
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-21 16:45:25 +0100

    Update criminal.js

--- charges/criminal.js
+++ charges/criminal.js
@@ -15,7 +15,20 @@ let argembed = new Discord.RichEmbed()
   return; 
   }
 
-message.reply("If you are looking for \'Criminal Accomplice Clause\' type: !criminal accomplice clause");
+  if(args[0] === "fines"){
+
+    let argembed = new Discord.RichEmbed()
+      .setTitle("**Criminal Fines (12)16 Info**")
+      .setColor("BLACK")
+      .addField("Information", "The Department of Justice may seek Criminal Fines through the State of San Andreas Court, to accompany imprisonment, for all felony and misdemeanor charges. The court shall impose a fine in all cases, except where the defendant establishes that he is unable to pay and is not likely to become able to pay any fine. A person who articulates and furnishes financial documentation to indicate financial difficulties with paying the fine shall be afforded additional time to pay the fine through a deadline established by the courts or through a payment plan to the Department of Justice. The fines may stack. ");
+    
+      message.channel.send(argembed);
+    
+      return; 
+      }
+    
+
+message.reply("If you are looking for \'Criminal Accomplice Clause\' type: !criminal accomplice clause. If you are looking for \'Criminal Fines\' type: !criminal fines.");
 
 }
 
