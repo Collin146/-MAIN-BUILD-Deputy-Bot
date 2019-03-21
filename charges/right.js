b1f75ce7a9896d50585e9f7388eaa9bf03f47674
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-21 18:54:27 +0100

    Create right.js

---
+++ charges/right.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+if(args[1] === "way"){
+
+let intimembed = new Discord.RichEmbed()
+.setTitle("**Right Of Way (11.5)06 Info**")
+.setColor("BLACK")
+.addField("Type of Punishment", "infraction")
+.addField("Punishment(s)", "$234");
+
+message.channel.send(intimembed);
+
+return;
+}
+
+message.reply("If you are looking for \'Right Of Way\' type: !right of way.")
+
+}
+
+module.exports.help = {
+    name: "right"
+}
