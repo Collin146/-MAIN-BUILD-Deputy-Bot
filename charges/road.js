9cfdd557bc967d020cfad2a0ea45feac7d827c45
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-21 19:22:29 +0100

    Create road.js

---
+++ charges/road.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+if (args[0] === "rage"){
+
+let intimembed = new Discord.RichEmbed()
+.setTitle("**Vehicle Lights (11.5)17 Info**")
+.setColor("BLACK")
+.addField("Type of Punishment", "Infraction")
+.addField("Punishment(s)", "$340")
+.addField("Information", "Does not stack with reckless driving");
+
+message.channel.send(intimembed);
+
+return;
+}
+
+message.reply("If you are looking for \'Road Rage\' type: !road rage");
+ 
+}
+
+module.exports.help = {
+    name: "road"
+}
