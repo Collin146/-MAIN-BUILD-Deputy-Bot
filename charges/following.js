f1775f6e59b8dab10c97e8aa8383c7acb6610931
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-21 19:09:52 +0100

    Update following.js

--- charges/following.js
+++ charges/following.js
@@ -8,7 +8,7 @@ if(args[0] === "distance"){
 let intimembed = new Discord.RichEmbed()
 .setTitle("**Following Distance (11.5)05 Info**")
 .setColor("BLACK")
-.addField("Type of Punishment", "infraction")
+.addField("Type of Punishment", "Infraction")
 .addField("Punishment(s)", "$234");
 
 message.channel.send(intimembed);
