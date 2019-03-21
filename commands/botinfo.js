8007642e964d4bd3cc2a63fd8c1af3bb6225e36e
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-21 09:43:54 +0100

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -3,12 +3,12 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
+    .setTitle("**Bot Information**")
     .setColor("#ff6a00")
     .setThumbnail(bicon)
     .addField("Bot Name", bot.user.username)
     .addField("Created On", bot.user.createdAt)
-    .addField("This bot was created and developed by:", "Collin#6966")
+    .addField("This bot was created and developed by:", "Collin A.#6966")
     return message.channel.send(botembed);
 
 
