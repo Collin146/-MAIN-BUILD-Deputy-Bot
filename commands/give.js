02d15c4b53586757f4b54d85c089b42716b45e78
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-26 15:38:23 +0100

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -3,10 +3,11 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+const collin = client.fetchUser('292598566759956480');
+
 let tobcso = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 if(!tobcso) return message.reply("Couldn't find that user.");
-if (!message.author.id === "292598566759956480") return;
-if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+if (!message.author.id === "292598566759956480") return collin.send(`${message.author} tried using the !give command!`);
 // let role = args[1];
 // if(!role) return tobcso.send("You didn't specify a role.");
 let memberrole = message.guild.roles.find(`name`, `Owner`);
