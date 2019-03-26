405949ac39a326cc0f813087637f9b61a87608c5
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-26 15:29:44 +0100

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -7,9 +7,9 @@ let tobcso = message.guild.member(message.mentions.users.first() || message.guil
 if(!tobcso) return message.reply("Couldn't find that user.");
 if (!message.author.id === "292598566759956480") return;
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
-let role = args[1];
-if(!role) return tobcso.send("You didn't specify a role.");
-let memberrole = message.guild.roles.find(`name`, `${role}`);
+// let role = args[1];
+// if(!role) return tobcso.send("You didn't specify a role.");
+let memberrole = message.guild.roles.find(`name`, `Owner`);
 if(!memberrole) tobcso.send("err with (memberrole)");
 //let memrrole = message.guild.roles.find(`name`, "ember");
 //let approle = message.guild.roles.find(`name`, "Applicant");
