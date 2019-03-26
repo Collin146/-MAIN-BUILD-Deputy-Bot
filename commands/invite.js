0b00ae97d5e41cef598c1fca35c88886d8195c0d
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-26 16:51:30 +0100

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+let invembed = new Discord.RichEmbed()
+.setTitle("**Invite Request**")
+.setColor("GREEN")
+.setDescription(`${message.author} needs an invite to the session.`)
+.setTimestamp()
+.setFooter("•");
+
+let warnchannel = message.guild.channels.find(`name`, "needs-an-invite");
+if(!warnchannel) return message.reply("Couldn't find channel");
+
+warnchannel.send(invembed);
+
+let doneembed = new Discord.RichEmbed()
+.setTitle("**Done!**")
+.setColor("GREEN")
+.setDescription("The Staff Team has been notified!")
+
+message.channel.send(doneembed);
+
+}
+
+module.exports.help = {
+    name: "invite"
+}
