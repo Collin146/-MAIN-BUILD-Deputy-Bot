1d3cc7ee80bbd648e0c5f9117fc2e74f3a873b5b
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-27 14:22:40 +0100

    Create suggest.js

---
+++ commands/suggest.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+let kReason = args.join(" ").slice(22);
+if (!kReason) return message.reply("Please give a suggestion");
+
+let sugembed = new Discord.RichEmbed()
+.setTitle("**New Suggestion!**")
+.setColor("GREEN")
+.setAuthor(`${message.author}`)
+.setDescription(`${kReason`)
+.setTimestamp()
+
+let warnchannel = message.guild.channels.find(`name`, "server-suggestions-staff-only");
+if(!warnchannel) return message.reply("Couldn't find channel");
+    
+warnchannel.send(sugembed).then(sentMessage => {
+let emoji1 = message.guild.find(`name`, "heavy_check_mark");
+let emoji2 = message.guild.find(`name`, "heavy_minus_sign");
+let emoji3 = message.guild.find(`name`, "heavy_multiplication_x");
+sentMessage.react(emoji1);
+sentMessage.react(emoji2);
+sentMessage.react(emoji3);
+
+});
+
+let doneembed = new Discord.RichEmbed()
+.setTitle("**Done!**")
+.setColor("GREEN")
+.setDescription("Your suggestion has been sent to the Staff Team!");
+
+channel.send(doneembed);
+
+}
+
+module.exports.help = {
+    name: "suggest"
+}
