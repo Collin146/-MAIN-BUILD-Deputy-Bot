55bcd0140033a368a9172d247c6a0e26a55b8715
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-27 16:40:34 +0100

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,29 +3,33 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
-
 let kReason = args[0];
 if (!kReason) return message.reply("Please give a suggestion");
 
 let sugembed = new Discord.RichEmbed()
 .setTitle("**New Suggestion!**")
 .setColor("GREEN")
-.setAuthor(`${message.author}`)
-.setDescription(`${kReason}`)
-.setTimestamp()
+.setAuthor(`${message.author.username}`, message.author.avatarURL)
+.setDescription(`${kReason}`);
 
-let warnchannel = message.guild.channels.find(`name`, "server-suggestions-staff-only");
+let warnchannel = message.guild.channels.find(`name`, "server-suggestions-staff-only"); 
 if(!warnchannel) return message.reply("Couldn't find channel");
-    
-warnchannel.send(sugembed).then(sentMessage => {
-let emoji1 = message.guild.find(`name`, "heavy_check_mark");
-let emoji2 = message.guild.find(`name`, "heavy_minus_sign");
-let emoji3 = message.guild.find(`name`, "heavy_multiplication_x");
-sentMessage.react(emoji1);
-sentMessage.react(emoji2);
-sentMessage.react(emoji3);
 
-});
+warnchannel.send(sugembed)
+.then(sentMessage => sentMessage.react('✔'))
+.then(sentMessage => sentMessage.react('➖'))
+.then(sentMessage => sentMessage.react('❌'))
+.catch(() => console.error('Failed to react to msg.'));
+
+// warnchannel.send(sugembed).then(sentMessage => {
+// let emoji1 = message.guild.find(`name`, "heavy_check_mark");
+// let emoji2 = message.guild.find(`name`, "heavy_minus_sign");
+// let emoji3 = message.guild.find(`name`, "heavy_multiplication_x");
+// sentMessage.react(emoji1);
+// sentMessage.react(emoji2);
+// sentMessage.react(emoji3);
+
+// });
 
 let doneembed = new Discord.RichEmbed()
 .setTitle("**Done!**")
