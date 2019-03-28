bb166ee5a7ef02d87972d1c283e9a03eea4c90f0
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-28 13:07:31 +0100

    Create r.js

---
+++ commands/r.js
@@ -0,0 +1,35 @@
+
+const Discord = require("discord.js");
+const ms = require("ms");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+    if(args[0] === "help"){
+        message.reply("Usage: !r");
+        return;
+    }
+    
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+    if(args[0] == "help"){
+        message.reply("Usage: !r");
+        return;
+    }
+
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+let mentionrole = message.guild.roles.find(`name`, `Member`);
+
+    message.channel.send([
+        `<@&${mentionrole.id}>`,
+        ` `,
+        "⚠ **Reminder!** ⚠"
+      ].join('\n'))
+
+ message.delete().catch(O_o=>{});
+
+}
+
+module.exports.help = {
+    name: "r"
+}
