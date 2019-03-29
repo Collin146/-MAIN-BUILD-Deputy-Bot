51a1e1e298939fc6067eb79bc51a603f12be77e3
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-03-29 09:11:17 +0100

    Update strike2.js

--- commands/strike2.js
+++ commands/strike2.js
@@ -20,7 +20,7 @@ module.exports.run = async (bot, message, args) => {
 
 let tostrike = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 if(!tostrike) return message.reply("Couldn't find that user.");
-let kReason = args.join(" ").slice(22);
+let kReason = args.join(" ").slice(21);
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
 if(tostrike.hasPermission("ADMINISTRATOR")) return message.reply("You cannot strike a Moderator or higher");
 let strikerole = message.guild.roles.find(`name`, "Strike 2");
