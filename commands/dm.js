6a628f98ca7ca535d0559f51426542cbc9dbcffe
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-04-01 15:35:20 +0200

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -16,15 +16,38 @@ module.exports.run = async (bot, message, args) => {
     }
 
 let reason = args.join(" ");
+if(!reason) return message.reply("Please provide the message that you want to send!");
+const yes = bot.emojis.get("561106357131018273");
+const no = bot.emojis.get("561106624757104640");
 
 let dmembed = new Discord.RichEmbed()
 .setTitle(`A message from ${message.guild.name}.`)
-.setColor("#ff6a00")
+.setColor("#00fff6")
 .setDescription(`${reason}`);
 
 message.guild.members.forEach(member => {
-      if (member.id != bot.user.id && !member.user.bot) member.send(dmembed);
+if (member.id != bot.user.id && !member.user.bot) member.send(dmembed);
     });
+
+let doneembed = new Discord.RichEmbed()
+.setTitle(`${yes.id} **Done!**`)
+.setColor("GREEN")
+.setDescription("A message has been sent to everyone in this server.");
+
+message.channel.send(doneembed);
+
+let ModEmbed = new Discord.RichEmbed()
+.setTitle("Dm command used!")
+.setColor("RED")
+.addField("Command Used In", message.channel, true)
+.addField("Command Used By", message.author.username, true)
+.setTimestamp()
+.setFooter(`Message ID: ${message.id} | Author ID: ${message.author.id}`);
+
+let warnchannel = message.guild.channels.find(`name`, "modlog");
+if(!warnchannel) return message.reply("Couldn't find channel");
+    
+warnchannel.send(ModEmbed);
 
 }
 
