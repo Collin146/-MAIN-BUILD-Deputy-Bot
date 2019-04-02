c040082d8313047246cf10f4e496e7049c9b1bcf
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-04-02 13:01:00 +0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -4,39 +4,36 @@ module.exports.run =async (bot, message, args) => {
     let inline = true
     let resence = true
     const status = {
-        online: "<:online:424890369688469504> Online",
-        idle: "<:idle:424890472855502849> Idle",
-        dnd: "<:dnd:424890429524410368> Do Not Disturb",
-        offline: "<:offilne:424890400319340546> Offline/Invisible"
+        online: "<:online:562588221121232907> Online",
+        idle: "<:idle:562588270903427072> Idle",
+        dnd: "<:dnd:562588318684938240> Do Not Disturb",
+        offline: "<:offline:562588687884484608> Offline/Invisible"
       }
         
 const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
 let target = message.mentions.users.first() || message.author
 
 if (member.user.bot === true) {
-    bot = "<:bottag:425631858265423883> Yes";
+    bot = "Yes";
   } else {
-    bot = "<:user:424958082691629057> No";
+    bot = "No";
   }
 
             let embed = new Discord.RichEmbed()
                 //.setAuthor(member.user.username)
+.setTitle(`${status[member.user.presence.status]} **${member.user.tag}'s Information`)
                 .setThumbnail((target.displayAvatarURL))
-                .setColor("#00ff00")
+                .setColor(message.guild.member(member).highestRole.color)
                 .addField("Full Username", `${member.user.tag}`, inline)
                 .addField("ID", member.user.id, inline)
-                .addField("Nickname", `${member.nickname !== null ? `<:yes:425632265993846795> Nickname: ${member.nickname}` : "<:no:425632070036094986> None"}`, true)
-                .addField("Bot", `${bot}`,inline, true)
-                .addField("Status", `${status[member.user.presence.status]}`, inline, true)
-                .addField("Playing", `${member.user.presence.game ? `🎮 ${member.user.presence.game.name}` : "<:no:425632070036094986> Not playing"}`,inline, true)
-                .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "<:no:425632070036094986> No Roles"}`, true)
+                .addField("Nickname", `${member.nickname !== null ? `<:yes:425632265993846795> Nickname: ${member.nickname}` : "None"}`, true)
+                .addField("Bot", `${bot}`, inline, true)
+                //.addField("Status", `${status[member.user.presence.status]}`, inline, true)
+                .addField("Playing", `${member.user.presence.game ? `${member.user.presence.game.name}` : "Not playing"}`,inline, true)
                 .addField("Joined Discord At", member.user.createdAt)
-                .setFooter(`Information about ${member.user.username}`)
-                .setTimestamp()
-    
-            message.channel.send(embed);
+        .addField(`Roles:`, message.guild.member(member).roles.map(s => s).join(" | "), true)
 
-            message.delete();
+            message.channel.send(embed);
     }
 
     module.exports.help = {
