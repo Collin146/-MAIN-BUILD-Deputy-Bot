c5aa0c220b6e2eb4b164a329c148cfbc6ff0572b
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-04-03 09:31:25 +0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -10,7 +10,7 @@ module.exports.run =async (bot, message, args) => {
         offline: "<:Offline:562637080526716939>"
       }
         
-const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
+const member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]) || message.member);
 let target = message.mentions.users.first() || message.author
 // const now = new Date[0](member.user.createdAt);
 // var  mydate = new Date(member.user.createdAt.startDate.value);
