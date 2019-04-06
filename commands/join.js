db45a0c33bf78d657da22036cf1fe7b2ffe0b8e9
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-04-06 23:16:27 +0200

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -3,13 +3,15 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
-message.member.voiceChannel.join()
+  if (!message.guild) return;
 
-// const ytdl = require('ytdl-core-discord');
- 
-// async function play(connection, url) {
-//   connection.playOpusStream(await ytdl(url));
-// }
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+});
 
 }
 
