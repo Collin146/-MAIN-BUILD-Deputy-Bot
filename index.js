9eb398dcc828791c97ec06b3e48f624f6ccd9c0a
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-04-06 23:15:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -247,25 +247,6 @@ bot.on(`message`, async message => {
 //Banned Words End
 //--
 
-//--
-// VC start
-//--
-
-bot.on('message', async message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
-
-  if (message.content === '/join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voice.channel) {
-      const connection = await message.member.voice.channel.join();
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
-
 //—
 //Channel Created Log Start
 //—
