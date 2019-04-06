7515d7604bd306437cc3449e2c9c35ab20f6fdc0
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-04-06 23:02:56 +0200

    Create join.js

---
+++ commands/join.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+message.member.voiceChannel.join()
+
+// const ytdl = require('ytdl-core-discord');
+ 
+// async function play(connection, url) {
+//   connection.playOpusStream(await ytdl(url));
+// }
+
+}
+
+module.exports.help = {
+    name: "join"
+}
