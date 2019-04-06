5837d2de292d46800c6f57a7694625e9f66724ad
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-04-06 23:21:40 +0200

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   if (!message.guild) return;
 
     // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voice.channel) {
+    if (message.member.voiceChannel) {
       const connection = await message.member.voiceChannel.join();
     } else {
       message.reply('You need to join a voice channel first!');
