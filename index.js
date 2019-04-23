fd464b1a1db1ec8a1c61055a11d1f672f6de6008
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-04-23 12:45:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -445,7 +445,7 @@ bot.on('messageUpdate', (oldMessage, newMessage) => {
     .setTimestamp()
     .setTitle("**Message Edited!**")
     .setDescription(`<@${oldMessage.author.id}>'s **message has been edited in** ${oldMessage.channel}`)
-    .addField("Before", `${oldMessage}`)
+    .addField("Before", `${oldMessage.content}`)
     .addField("After", `${newMessage}`);
 
 // let modlogchannel = guild.channels.find(`name`, "modlog");
