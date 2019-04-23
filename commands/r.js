4daf9d7ae13b882935da796fc110cfb7bdeaaac7
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-04-23 21:34:31 +0200

    Update r.js

--- commands/r.js
+++ commands/r.js
@@ -1,4 +1,3 @@
-
 const Discord = require("discord.js");
 const ms = require("ms");
 const errors = require("../utils/errors.js");
@@ -19,9 +18,10 @@ module.exports.run = async (bot, message, args) => {
 
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
 let mentionrole = message.guild.roles.find(`name`, `Member`);
+let traineerole = message.guild.roles.find("name", "Trainee");
 
     message.channel.send([
-        `<@&${mentionrole.id}>`,
+        `<@&${mentionrole.id || traineerole.id}>`,
         ` `,
         "⚠ **Reminder!** ⚠"
       ].join('\n'))
