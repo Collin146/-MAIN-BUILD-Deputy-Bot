3245f24945aaf1d727b7d38876c54eab9f1a2e8f
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-04-28 23:26:16 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -15,6 +15,36 @@ module.exports.run = async (bot, message, args) => {
         return;
     }
     
+    if (args[0] > 100) {
+
+        let args100 = args[0] - 100
+    message.channel.bulkDelete(100)
+    message.channel.bulkDelete(args100).then(() => {
+        message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+
+        let ModEmbed = new Discord.RichEmbed()
+        .setTitle("**Moderation Command Used!**")
+        .setTimestamp()
+        .setColor("BLACK")
+        .setDescription([
+            `**The moderation command** !clear **has been used**`,
+            ` `,
+            `**Cleared Amount:** ${args}`,
+            ` `,
+            `**Used In:** ${message.channel}`,
+            ` `,
+            `**Used By:** ${message.author.username}`
+          ].join('\n'))
+        .setFooter(`Message ID: ${message.id} | Author ID: ${message.author.id}`);
+        
+        let warnchannel = message.guild.channels.find(`name`, "modlog");
+        if(!warnchannel) return message.reply("Couldn't find channel");
+        
+        warnchannel.send(ModEmbed);
+
+return;
+    });
+}
 
     //!clear 15
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have permission to do that.");
@@ -23,16 +53,6 @@ module.exports.run = async (bot, message, args) => {
         message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
 
     });
-
-    if (args[0] > 100) {
-
-        let args100 = args[0] - 100
-    message.channel.bulkDelete(100)
-    message.channel.bulkDelete(args100).then(() => {
-        message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
-
-    });
-}
 
     let ModEmbed = new Discord.RichEmbed()
     .setTitle("**Moderation Command Used!**")
