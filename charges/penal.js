d2dcbcf4ad30aaf76c88f0a0ae98add7d33b650b
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-05-03 22:08:22 +0200

    Create penal.js

---
+++ charges/penal.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+    if(args[1] === "help"){
+
+        let intimembed = new Discord.RichEmbed()
+        .setTitle("**Penal Code Commands Help Menu**")
+        .setColor("WHITE")
+        .addField("!titles", "This shows all of the titles & the commands to see all of the charges of a specific title.")
+        .addField("!title <number>", "This shows all of the charges which fall under the title number that is put in & the commands to see each specific charge.")
+        .addField("!<charge>", "This shows the information about the charge that is put in.");
+        
+        message.channel.send(intimembed);
+        
+            }
+
+            message.reply("If you were looking for the Penal Code Commands Help menu type: !penal code help.");
+}
+
+module.exports.help = {
+    name: "penal"
+}
