45c1cec07ee55e4dafe043fcedc0fbd33209cb21
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-07-27 14:27:55 +0200

    Create leave.js

---
+++ commands/leave.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+client.guilds.get(args.join("")).leave()
+
+}
+
+module.exports.help = {
+    name: "leave"
+}
