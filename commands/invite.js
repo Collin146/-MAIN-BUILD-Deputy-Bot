ec9e9ddd84e74c540c1c62bedd75a469c5ea06a2
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-08-05 21:27:58 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
-let mentionrole = message.guild.roles.find(`name`, `Staff Team`);
+let mentionrole = message.guild.roles.find(x => x.name === 'Staff Team');
 const yes = bot.emojis.get("561106357131018273");
 const no = bot.emojis.get("561106624757104640");
 
@@ -13,11 +13,9 @@ let invembed = new Discord.RichEmbed()
 .setDescription(`${message.author} needs an invite to the session.`)
 .setTimestamp()
 
-let warnchannel = message.guild.channels.find(`name`, "needs-an-invite");
-if(!warnchannel) return message.reply("Couldn't find channel");
-
-warnchannel.send(`<@&${mentionrole.id}>`)
-await warnchannel.send(invembed);
+let modlogchannel = message.guild.channels.find(x => x.name === 'modlog');
+modlogchannel.send(`<@&${mentionrole.id}>`)
+await modlogchannel.send({embed: invembed});
 
 let doneembed = new Discord.RichEmbed()
 .setTitle(`${yes} **Done!**`)
