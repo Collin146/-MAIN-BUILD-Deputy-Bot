cc00e236948f77317c169f7920c4fc38bbd54320
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-08-05 21:52:19 +0200

    Update r.js

--- commands/r.js
+++ commands/r.js
@@ -17,7 +17,8 @@ module.exports.run = async (bot, message, args) => {
     }
 
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
-let mentionrole = message.guild.roles.find(`name`, `Member`);
+let mentionrole = message.guild.roles.find(x => x.name === 'Member');
+
 
     message.channel.send([
         `<@&${mentionrole.id}>`,
