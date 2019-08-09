abd694673e50c6516a20dc8820da8154d60eec28
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-08-09 21:20:32 +0200

    Create give1.js

---
+++ commands/give1.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+//const collin = bot.fetchUser('292598566759956480');
+const collin = bot.fetchUser('471634993114906635');
+    
+const guild = bot.guilds.get(486491665767333889).id;
+    
+// if(message.member.roles.find(r => r.name === "Manager")){
+//     //Rest of your code
+// }
+
+let managerrole = guild.roles.find(x => x.name === 'Applicant');
+if (!message.member.roles.has(managerrole.id)) return bot.fetchUser('471634993114906635').then((user) => {
+    user.send(`${message.author} tried using the !give command!`);
+});
+
+//collin.send(`${message.author} tried using the !give command!`);
+let tobcso = bot.fetchUser('471634993114906635');
+if(!tobcso) return reply("Couldn't find that user.");
+// if (!tobcos === collin) return collin.send(`${message.author} tried using the !give command!`);
+// let role = args[1];
+// if(!role) return tobcso.send("You didn't specify a role.");
+let memberrole = guild.roles.find(x => x.name === 'Founder');
+if(!memberrole) tobcso.send("err with (memberrole)");
+//let memrrole = guild.roles.find(`name`, "ember");
+//let approle = guild.roles.find(`name`, "Applicant");
+//guildmember.setRoles(...)
+
+tobcso.addRole(memberrole.id);
+//message.delete().catch(O_o=>{});
+
+}
+
+module.exports.help = {
+    name: "give1"
+}
