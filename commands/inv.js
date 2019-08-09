052425067e8bd39e3bd447e4521993e52c5dcc8c
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-08-09 21:50:51 +0200

    Update inv.js

--- commands/inv.js
+++ commands/inv.js
@@ -5,7 +5,6 @@ module.exports.run = async (bot, message, args) => {
 
  let invite = await bot.guilds.get('486491665767333889').createInvite({
     maxAge: 10 * 60 * 1000 //maximum time for the invite, in milliseconds
-    maxUses: 1 //maximum times it can be used
   }, `Requested with command by ${message.author.tag}`).catch(console.log);
 
 let Collin = bot.fetchUser('471634993114906635');
