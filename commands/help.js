757972326c5b4041147cc320683906975e4bf3aa
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-10-08 19:35:43 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,64 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-let helpembed = new Discord.RichEmbed()
-.setTitle("**Commands Help Menu**")
-.setColor("#00fff6")
-.addField("!botinfo", "This shows you the information about the bot.")
-.addField("!help", "This brings up the commands menu.")
-.addField("!report <user> <reason>", "This reports the mentioned user to staff. ALWAYS GIVE A REASON!")
-.addField("!serverinfo", "This shows you the information about the server.")
-.addField("!ticket <reason>", "This sends a ticket to the Staff Team with the reason you gave.")
-.addField("!suggest <your suggestion>", "This sends the suggestion you give to the Staff Team.")
-.addField("!invite", "This sends a message to the Staff Team telling them that you need an invite to the session that is active at that time.");
+    geluktEmbed = new Discord.RichEmbed()
+    .setColor("GREEN")
+    .setTitle(`${yes} **Command List**`)
+    .setDescription(`Here is the command list of this bot: \https://docs.google.com/document/d/10m06EtPOt64mJ86K2IFeXH5c5tVN-F2bZ9r8x4ikc5o/edit?usp=sharing\ `)
 
-message.channel.send(helpembed);
-
-if(message.member.hasPermission("MANAGE_MESSAGES")){
-let modembed = new Discord.RichEmbed()
-.setTitle("**Staff Team Help Menu**")
-.setColor("#00fff6")
-.addBlankField(true)
-.addField("**---------Moderation Commands---------**", "The basic moderation commands.")
-.addField("!ban <user> <reason>", "This bans the user that is mentioned.") //PUT CLEAR, PREFIX, TEMPMUTE IF VIDEO DOESN'T
-.addField("!kick <user> <reason>", "This kicks the user that is mentioned.")
-.addField("!warn <user> <reason>", "This warns the user that is mentioned.")
-.addField("!strike <1 or 2> <user> <reason>", "This adds the Strike 1 or 2 role (depending on what number you provided) role to the mentioned user and sends them a DM telling them that they have been striked.")
-.addField("!watchlist <user> <reason>", "This adds the Watchlist role to the mentioned user and sends them a DM telling them that they have been put on watchlist.")
-.addField("!investigation <user>", "This removes all roles that a user has and adds the investigation role.")
-.addField("!tempmute <user> <time length>", "This temporarily mutes the mentioned user for the given amount of time.")
-.addField("!unmute <user>", "This command unmutes a user who has been muted by the !tempmute command.")
-.addField("!dm <message>", "This sends a DM to all of the users that are in the guild that this command is used in with the message that is provided.")
-.addBlankField(true)
-.addField("**---------Managing Commands---------**", "The basic managing commands.")
-.addField("!prefix <new prefix>", "This changes the prefix to the desired one.")
-.addField("!clear <amount>", "This deletes the given amount of messages at once.")
-.addField("!bcso <user>", "This adds the basic BCSO roles to the mentioned user.")
-.addField("!sahp <user>", "This adds the basic SAHP roles to the mentioned user.")
-.addField("!civilian <user>", "This adds the basic Civilian roles to the mentioned user.")
-.addField("!lockdown", "This overrides the channel it's send messages permission to off for everyone")
-.addField("!lockdown unlock", "This overrides the channel it's send messages permission to on for everyone")
-.addField("!userinfo <user>", "This shows the mentioned user it's account information")
-.addField("!reject <user> <reason>", "This sends a DM to the mentioned user telling them that their application form was rejected and the reason why");
-
-let modembed2 = new Discord.RichEmbed()
-.addField("**---------Other Commands---------**", "The rest of the commands.")
-.setColor("#00fff6")
-.addField("!say <any word or sentence>", "This makes the bot say the given word or sentence.")
-.addField("!session <day> <time>", "This announces a session for the given day and time.")
-.addField("!training <day> <time>", "This announces a training session for the given day and time.")
-.addField("!briefing <time until briefing>", "This will announce briefing when the given time has ended.")
-.addField("!r", "This mentions everyone and sends a reminder message in the channel that the command was used in.")
-.addField("!ping", "This shows the latency between the bot responding to commands.");
-try{
-    await message.author.send(modembed);
-    await message.author.send(modembed2);
-    message.react("👍");
-}catch(e){
-    message.reply("Your DMs are locked. I cannot send you the mod commands.");
-
- }
-}
+    message.channel.send(geluktEmbed);
 
 }
 
