445d8784a8c5d8571acb5c23020ca28ca368cf91
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-10-09 12:53:16 +0200

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -18,10 +18,34 @@ module.exports.run = async (bot, message, args, channel) => {
     // let bUser = message.guild.members.get() || Client.fetchUser();
     //if(!bUser) return message.channel.send("Can't find user!");
     let bReason = args.slice(1).join(" ");
-    let bUser = args[0];
+    let user: User;
     // if(!bReason) return message.reply("Please give a reason.");
     const yes = bot.emojis.get("561106357131018273");
     const no = bot.emojis.get("561106624757104640");
+    
+    if (args[0]) {
+			// Check if it's a user ID
+			if (idRegex.test(args[0])) {
+				try { user = await message.client.fetchUser(args[0].match(idRegex)[0]); }
+				catch (err) { return message.channel.send(`Could not locate user **${args[0]}** from ID argument.`); }
+			}
+		} else {
+			return message.channel.send(`No users found. Please specify a User ID.`);
+		}
+
+let banned: boolean = await message.guild.fetchBans().then(bans => {
+				let users = bans.filter(r => r === user);
+				if (!users.first()) {
+					message.channel.send('User is not banned.');
+					message.delete();
+					return Promise.resolve(false);
+				} else {
+					return Promise.resolve(true);
+				}
+			});
+
+			if (!banned) { return; }
+
 
 let geluktEmbed = new Discord.RichEmbed()
       .setColor("GREEN")
@@ -39,7 +63,9 @@ let geluktEmbed = new Discord.RichEmbed()
    // .addField("Time", message.createdAt)
    // .addField("Reason", bReason);
 
-    message.guild.unban(bUser).unban(bReason || "None");
+    //message.guild.unban(bUser).unban(bReason || "None");
+    
+    this.client.mod.actions.unban(user, moderator, message.guild, note);
     message.channel.send(geluktEmbed);
 
     let ModEmbed = new Discord.RichEmbed()
