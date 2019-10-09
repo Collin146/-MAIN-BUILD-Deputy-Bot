db521d3c07e7f4da812e42593bd1a89bb6a13747
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-10-09 18:42:11 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -5,41 +5,38 @@ module.exports.run = async (bot, message, args, channel) => {
     
     if(!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "ADMINISTRATOR");
     if(args[0] === "help"){
-        message.reply("Usage: !ban <user> <reason>");
+        message.reply("Usage: !ban <user> <reason");
         return;
     }
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You don't have permission to do that.");
     if(args[0] == "help"){
-        message.reply("Usage: !ban <user> <reason");
+        message.reply("Usage: !ban <user> <reason>");
         return;
     }
+    
+let user = args[0];
+const usercheck = bot.users.get(user) || message.guild.member(message.mentions.users.first())
+if (!usercheck) return message.channel.send("Couldn't find this user.")
+const yes = bot.emojis.get("561106357131018273");
+const no = bot.emojis.get("561106624757104640");    
+let bReason = args.slice(1).join(" ");
+const username = bot.fetchUser(user)
 
-    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]) || bot.fetchUser(args[0]));
-    // let bUser = message.guild.members.get() || Client.fetchUser();
-    if(!bUser) return message.channel.send("Can't find user!");
-    let bReason = args.slice(1).join(" ");
-    if(bUser.hasPermission("ADMINISTRATOR")) return message.channel.send("You cannot ban an Admin.");
-    // if(!bReason) return message.reply("Please give a reason.");
-    const yes = bot.emojis.get("561106357131018273");
-    const no = bot.emojis.get("561106624757104640");
+message.guild.fetchBans().then(bans => {
+            bans.forEach(user => {
+                console.log(user.username + '#' + user.tag);
+                message.guild.ban(user);
+                // if (!user) return message.channel.send("Couldn't find this user!")
+            });
+        });
+
 
 let geluktEmbed = new Discord.RichEmbed()
       .setColor("GREEN")
       .setTitle(`${yes} **Done!**`)
-      .setDescription(`<@${bUser.id}> has been banned!`)
-      .setFooter(`Mentioned User ID: ${bUser.id}`);
+      .setDescription(`<@${user}> has been banned!`)
+      .setFooter(`Mentioned User ID: ${user}`);
 
-
-   // let banEmbed = new Discord.RichEmbed()
-   // .setDescription("A user has been banned")
-   // .setColor("#ff6a00")
-   // .addField("Banned User", `${bUser} with ID: ${bUser.id}`)
-   // .addField("Banned By", `<@${message.author.id}> with ID: ${message.author.id}`)
-   // .addField("Banned In", message.channel)
-   // .addField("Time", message.createdAt)
-   // .addField("Reason", bReason);
-
-    message.guild.member(bUser).ban(bReason || "None");
     message.channel.send(geluktEmbed);
 
     let ModEmbed = new Discord.RichEmbed()
@@ -49,7 +46,7 @@ let geluktEmbed = new Discord.RichEmbed()
     .setDescription([
         `**The administration command** !ban **has been used**`,
         ` `,
-        `**Banned User:** <@${bUser.id}>`,
+        `**Banned User:** <@${user}>`,
         ` `,
         `**Used In:** ${message.channel}`,
         ` `,
@@ -63,13 +60,12 @@ let modlogchannel = message.guild.channels.find(x => x.name === 'modlog');
 modlogchannel.send({embed: ModEmbed});
 
 
-    let dmembed =  new Discord.RichEmbed()
-    .setTitle(`**You have been banned from ${message.guild.name}.**`)
-    .setColor("#00fff6")
-    .addField("Reason:", bReason || "None");
+   // let dmembed =  new Discord.RichEmbed()
+  //  .setTitle(`**You have been unbanned from ${message.guild.name}.**`)
+  //  .setColor("#00fff6")
+  //  .addField("Reason:", bReason || "None");
 
-    bUser.send(dmembed);
-
+    //username.send(dmembed);
 }
 
  module.exports.help = {
