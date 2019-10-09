ddd973075018d40383c9a7af8a250cc595bf40c2
Collin146 <47307903+Collin146@users.noreply.github.com>
2019-10-09 13:06:45 +0200

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -13,65 +13,24 @@ module.exports.run = async (bot, message, args, channel) => {
         message.reply("Usage: !unban <user>");
         return;
     }
-
-    //let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]) || bot.fetchUser(args[0]));
-    // let bUser = message.guild.members.get() || Client.fetchUser();
-    //if(!bUser) return message.channel.send("Can't find user!");
-    let bReason = args.slice(1).join(" ");
-    const idRegex: RegExp = /^(?:<@!?)?(\d+)>?$/;
-    // if(!bReason) return message.reply("Please give a reason.");
-    const yes = bot.emojis.get("561106357131018273");
-    const no = bot.emojis.get("561106624757104640");
     
-    public async action(message: Message, args: Array<any>): Promise<any> {
-		let moderator: GuildMember = message.member;
-		let user: User;
+let bReason = args.slice(1).join(" ");
 
+message.guild.fetchBans().then(bans => {
+            bans.forEach(user => {
+                console.log(user.username + '#' + user.tag);
+                message.guild.unban(user);
+            });
+        });
+    });
 
-    if (args[0]) {
-			// Check if it's a user ID
-			if (idRegex.test(args[0])) {
-				try { user = await message.client.fetchUser(args[0].match(idRegex)[0]); }
-				catch (err) { return message.channel.send(`Could not locate user **${args[0]}** from ID argument.`); }
-			}
-		} else {
-			return message.channel.send(`No users found. Please specify a User ID.`);
-		}
-
-let banned: boolean = await message.guild.fetchBans().then(bans => {
-				let users = bans.filter(r => r === user);
-				if (!users.first()) {
-					message.channel.send('User is not banned.');
-					message.delete();
-					return Promise.resolve(false);
-				} else {
-					return Promise.resolve(true);
-				}
-			});
-
-			if (!banned) { return; }
-
-} 
 
 let geluktEmbed = new Discord.RichEmbed()
       .setColor("GREEN")
       .setTitle(`${yes} **Done!**`)
-      .setDescription(`<@${bUser.id}> has been unbanned!`)
-      .setFooter(`Mentioned User ID: ${bUser.id}`);
+      .setDescription(`<@${user.id}> has been unbanned!`)
+      .setFooter(`Mentioned User ID: ${user.id}`);
 
-
-   // let banEmbed = new Discord.RichEmbed()
-   // .setDescription("A user has been banned")
-   // .setColor("#ff6a00")
-   // .addField("Banned User", `${bUser} with ID: ${bUser.id}`)
-   // .addField("Banned By", `<@${message.author.id}> with ID: ${message.author.id}`)
-   // .addField("Banned In", message.channel)
-   // .addField("Time", message.createdAt)
-   // .addField("Reason", bReason);
-
-    //message.guild.unban(bUser).unban(bReason || "None");
-    
-    this.client.mod.actions.unban(user, moderator, message.guild, note);
     message.channel.send(geluktEmbed);
 
     let ModEmbed = new Discord.RichEmbed()
@@ -81,7 +40,7 @@ let geluktEmbed = new Discord.RichEmbed()
     .setDescription([
         `**The administration command** !unban **has been used**`,
         ` `,
-        `**Unbanned User:** <@${bUser.id}>`,
+        `**Unbanned User:** <@${user.id}>`,
         ` `,
         `**Used In:** ${message.channel}`,
         ` `,
@@ -100,11 +59,9 @@ modlogchannel.send({embed: ModEmbed});
     .setColor("#00fff6")
     .addField("Reason:", bReason || "None");
 
-    bUser.send(dmembed);
-// this is a test 133
+    user.send(dmembed);
 }
 
  module.exports.help = {
      name: "unban"
  }
-
