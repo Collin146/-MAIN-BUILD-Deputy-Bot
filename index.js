90634751994c75e63b268c15035a618769d68d7b
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-03-21 11:32:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -622,21 +622,21 @@ modlogchannel.send({embed: cuembed});
 
 //  GIVE ROLES THROUGH JOINING VC
 
-bot.on('voiceStateUpdate', (oldMember, newMember) => {
-  let newUserChannel = newMember.voiceChannel
-  let oldUserChannel = oldMember.voiceChannel
-  let memberrole = newMember.guild.roles.find(x => x.name === 'Founder');
+// bot.on('voiceStateUpdate', (oldMember, newMember) => {
+//   let newUserChannel = newMember.voiceChannel
+//   let oldUserChannel = oldMember.voiceChannel
+//   let memberrole = newMember.guild.roles.find(x => x.name === 'Founder');
 
-  if(oldUserChannel === undefined && newUserChannel !== undefined) {
+//   if(oldUserChannel === undefined && newUserChannel !== undefined) {
 
-newMember.addRole(memberrole.id);
+// newMember.addRole(memberrole.id);
 
-  } else if(newUserChannel === undefined){
+//   } else if(newUserChannel === undefined){
 
-return;
+// return;
 
-  }
-})
+//   }
+// })
 
 // bot.on('voiceStateUpdate', (oldMember, newMember) => {
 //   let newUserChannel = newMember.voiceChannel
