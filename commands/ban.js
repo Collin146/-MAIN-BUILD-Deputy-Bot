46203429f33266ac9ffe16ccf93758b6ec59b895
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-17 12:42:16 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
-function catchError (error, message);
+function catchError (error, message)
 
 module.exports.run = async (bot, message, args, channel) => {
     
