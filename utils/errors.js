625e7a4a2470a91633572345a2c010b918ebbd5c
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-17 13:27:42 +0200

    Update errors.js

--- utils/errors.js
+++ utils/errors.js
@@ -1,15 +1,15 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 let config = require("../Botconfig.json");
-// const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client({disableEveryone: true});
 
-module.exports.noPerms = async (bot, message, args, perm) => { 
+module.exports.noPerms = (message, perm) => {
 
 const no = bot.emojis.get("561106624757104640");   
 
     let embed = new Discord.RichEmbed()
 
-    .setTitle(`${no} **Error!**`)
+    .setTitle("❌ **Error!**")
     .setColor("RED")
     .setDescription("Insufficient permissions", perm);
 
