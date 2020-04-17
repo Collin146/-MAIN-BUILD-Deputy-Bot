1ccf07640cc9b27908d9faf7a135beeba5eca677
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-17 19:35:23 +0200

    Update training.js

--- commands/training.js
+++ commands/training.js
@@ -5,19 +5,35 @@ module.exports.run = async (bot, message, args) => {
 
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
     if(args[0] === "help"){
-        message.reply("Usage: !sahp <user>");
+        message.reply("Usage: !training <day> <time>");
         return;
     }
     
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
     if(args[0] == "help"){
-        message.reply("Usage: !sahp <user>");
+        message.reply("Usage: !training <day> <time>");
         return;
     }
-    
-let mentionrole = message.guild.roles.find(x => x.name === 'Trainee');
+
+const yes = bot.emojis.get("700713527576625205");
+const no = bot.emojis.get("700713478578634783");
+let mentionrole = message.guild.roles.find(x => x.name === 'Recruit');
 let day = args[0];
 let time = args[1];
+
+let errEmbed = new Discord.RichEmbed()
+.setColor("RED")
+.setTitle(`${no} **Error!**`)
+.setDescription(`You didn't provide a day & time!`);
+
+if (!day) return message.channel.send(errEmbed);
+
+let errEmbed2 = new Discord.RichEmbed()
+.setColor("RED")
+.setTitle(`${no} **Error!**`)
+.setDescription(`You didn't provide a time!`);
+
+if (!time) return message.channel.send(errEmbed2);
 
 message.channel.send([
     `<@&${mentionrole.id}>`,
