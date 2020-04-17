5fc21895352a5e7394b086977780c17272c84420
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-17 12:14:17 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -17,9 +17,15 @@ module.exports.run = async (bot, message, args, channel) => {
 //let user = args[0];
 //const usercheck = bot.users.get(user) || message.guild.member(message.mentions.users.first())
 const user = message.mentions.users.first() || await bot.fetchUser(args[0]);
-if (!user) return message.channel.send("Couldn't find this user.") // Change if not working
 const yes = bot.emojis.get("561106357131018273");
 const no = bot.emojis.get("561106624757104640");    
+
+let errEmbed = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription(`Was not able to find that user!`);
+
+if (!user) return message.channel.send(errEmbed) // Change if not working 
 let bReason = args.slice(1).join(" ");
 const username = bot.fetchUser(user)
 
