cd03064f9e9f37fb7db42268a66754853dda1fca
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-17 14:02:34 +0200

    Update briefing.js

--- commands/briefing.js
+++ commands/briefing.js
@@ -17,13 +17,19 @@ module.exports.run = async (bot, message, args) => {
     }
 
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
-    
+
+const yes = bot.emojis.get("561106357131018273");
+const no = bot.emojis.get("561106624757104640");
 let mentionrole = message.guild.roles.find(x => x.name === 'Member');
 //let mentionrole = message.guild.roles.find(`name`, `Member`);
 let mutetime = args[0];
-if(!mutetime) return message.reply("You didn't specify a time!");
-const yes = bot.emojis.get("561106357131018273");
-const no = bot.emojis.get("561106624757104640");
+
+let errEmbed = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("You didn't specify a time!");
+
+if(!mutetime) return message.channel.send(errEmbed);
 
 if(args[0] == "cancel"){
 
@@ -62,7 +68,13 @@ let dmembed = new Discord.RichEmbed()
 try{
     await message.author.send(dmembed);
 }catch(e){
-    message.reply("Your DMs are locked. I cannot send you the mod commands.");
+
+    let errEmbed2 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("Your DMs are locked. I cannot send you the confirmation message!");
+
+    message.channel.send(errEmbed2);
 
  }
  message.delete().catch(O_o=>{})
