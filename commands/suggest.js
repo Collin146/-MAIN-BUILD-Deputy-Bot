3f5bb7130114a2e84b20da4e3a4666123f2cfbe3
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-17 18:09:21 +0200

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,11 +3,17 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
-let kReason = args.join(" ").slice(0);
-if(!kReason) return message.reply("Please give a suggestion");
 const yes = bot.emojis.get("700713527576625205");
 const no = bot.emojis.get("700713478578634783"); 
 const maybe = bot.emojis.get("700713222566707203");
+let kReason = args.join(" ").slice(0);
+
+let errEmbed = new Discord.RichEmbed()
+.setColor("RED")
+.setTitle(`${no} **Error!**`)
+.setDescription(`You didn't provide your suggestion!`);
+
+if(!kReason) return message.channel.send(errEmbed);
 
 let sugembed = new Discord.RichEmbed()
 .setTitle("**New Suggestion!**")
@@ -16,7 +22,6 @@ let sugembed = new Discord.RichEmbed()
 .setDescription(`${kReason}`);
     
 let warnchannel = message.guild.channels.find(x => x.name === 'server-suggestions-staff-only');
-if(!warnchannel) return message.reply("Couldn't find channel");
 
 const sentMessage =  await warnchannel.send(sugembed);
 await sentMessage.react(yes.id);
