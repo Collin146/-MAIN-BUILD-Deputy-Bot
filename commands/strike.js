45a03c612a4d6966d7a82e4d541b713a272ebdff
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-17 17:39:45 +0200

    Update strike.js

--- commands/strike.js
+++ commands/strike.js
@@ -20,7 +20,7 @@ module.exports.run = async (bot, message, args) => {
 const yes = bot.emojis.get("700713527576625205");
 const no = bot.emojis.get("700713478578634783"); 
 //let tostrike = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-const tostrike = message.mentions.users.first() || await message.guild.fetchUser(args[0]);
+const tostrike = message.mentions.users.first() || await message.guildMember()
 
 let errEmbed = new Discord.RichEmbed()
 .setColor("RED")
