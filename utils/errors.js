2887a92fdd75723375fb111384b1b4d464e33cd5
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-17 13:08:20 +0200

    Update errors.js

--- utils/errors.js
+++ utils/errors.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 let config = require("../Botconfig.json");
 
-module.exports.noPerms = (message, perm) => {
+module.exports.noPerms = (bot, message, perm) => {
 
 const yes = bot.emojis.get("561106357131018273");
 const no = bot.emojis.get("561106624757104640");  
