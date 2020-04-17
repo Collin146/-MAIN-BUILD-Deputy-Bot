5861395a4758920bb3c91d33168358fb4ca05001
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-17 13:47:33 +0200

    Update bcso.js

--- commands/bcso.js
+++ commands/bcso.js
@@ -15,31 +15,56 @@ module.exports.run = async (bot, message, args) => {
         return;
     }
 
-let tobcso = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-if(!tobcso) return message.reply("Couldn't find that user.");
-if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
-let memberrole = message.guild.roles.find(x => x.name === 'Member');
-let bcsorole = message.guild.roles.find(x => x.name === 'BCSO');
-let probrole = message.guild.roles.find(x => x.name === 'BCSO Probationary');
-let leorole = message.guild.roles.find(x => x.name === 'LEO');
-let nmrole = message.guild.roles.find(x => x.name === 'New Member');;
-if(tobcso.hasPermission("ADMINISTRATOR")) return message.channel.send("You cannot use this command on an admin!");
 const yes = bot.emojis.get("561106357131018273");
 const no = bot.emojis.get("561106624757104640");
+let tobcso = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+let errEmbed = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("Was not able to find that user!");
+
+if(!tobcso) return message.channel.send(errEmbed);
+let memberrole = message.guild.roles.find(x => x.name === 'Member');
+let bcsorole = message.guild.roles.find(x => x.name === 'BCSO');
+let probrole = message.guild.roles.find(x => x.name === 'Probationary Deputy');
+let nmrole = message.guild.roles.find(x => x.name === 'New Member');
+
+let errEmbed2 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("You cannot use this command on an administrator.");
+
+if(tobcso.hasPermission("ADMINISTRATOR")) return message.channel.send(errEmbed2);
 //let memrrole = message.guild.roles.find(`name`, "ember");
 //let approle = message.guild.roles.find(`name`, "Applicant");
 //guildmember.setRoles(...)
 
-   if(!memberrole) return message.reply("The role \'Member\' doesn't exist");
-   if(!bcsorole) return message.reply("The role \'BCSO\' doesn't exist");
-   if(!probrole) return message.reply("The role \'BCSO Probationary\' doesn't exist");
-   if(!leorole) return message.reply("The role \'LEO\' doesn't exist");
+let errEmbed3 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("The role \'Member\' doesn't exist");
+
+   if(!memberrole) return message.channel.send(errEmbed3);
+
+let errEmbed4 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("The role \'BCSO\' doesn't exist");
+
+   if(!bcsorole) return message.channel.send(errEmbed4);
+
+let errEmbed5 = new Discord.RichEmbed()
+   .setColor("RED")
+   .setTitle(`${no} **Error!**`)
+   .setDescription("The role \'Probationary Deputy\' doesn't exist");
+
+   if(!probrole) return message.channel.send(errEmbed5);
     
 tobcso.removeRole(nmrole.id);
 tobcso.addRole(memberrole.id);
 tobcso.addRole(bcsorole.id);
 tobcso.addRole(probrole.id);
-tobcso.addRole(leorole.id);
 
 let ModEmbed = new Discord.RichEmbed()
 .setTitle("**Moderation Command Used!**")
