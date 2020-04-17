8694f64b61a7dc3a31ef67744975eddeeee8ff8e
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-17 19:20:35 +0200

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -11,38 +11,21 @@ const no = client.emojis.get("700713478578634783");
 //Embed voor geen reden
  let errorEmbed = new discord.RichEmbed()
   .setColor("RED")
-  .setTitle(`${no} **Error**`)
-  .setDescription("Please input a valid reason!")
+  .setTitle(`${no} **Error!**`)
+  .setDescription("You didn't provide a reason for this ticket!")
 
   if(!onderwerp) return message.channel.send(errorEmbed); //als er geen args zijn
 
   let role = message.guild.roles.find(x => x.name === 'Staff Team');
   let role2 = message.guild.roles.find(x => x.name ==='@Staff Team'); //De role van iedereen
-        // Als ticket al gemaakt is
-        var bool = false;
 
-        // Kijk na als ticket al gemaakt is.
-        message.guild.channels.forEach((channel) => {
 
-          // Als ticket is gemaakt, zend bericht.
-          if (channel.name == "!ticket-" + userName.toLowerCase()) {
+  let errEmbed2 = new discord.RichEmbed()
+  .setColor("RED")
+  .setTitle(`${no} **Error!**`)
+  .setDescription("Please create a role called **Staff Team** to use tickets!");
 
-            let dongembed = new discord.RichEmbed()
-            .setColor("RED")
-            .setTitle(`${no} **Error**`)
-            .setDescription("<:xcross:504361310385995798> You already have a open ticket!")
-            message.channel.send(dongembed);
-
-              bool = true;
-
-          }
-
-      });
-
-      // Als ticket return code.
-      if (bool == true) return;
-
-      if (!role) return message.channel.send("Please create a role called **Staff Team** to use tickets. ") //Als support rank er niet is
+      if (!role) return message.channel.send(errEmbed2); //Als support rank er niet is
       const ticketEmbed = new discord.RichEmbed()
       .setTitle("**New ticket!**")
       .addField("Ticket creator", `**${message.author}**`, true)
