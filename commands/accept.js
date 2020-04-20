54dd03305db62c985b3a4471b51ac48cd19080e9
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-20 12:22:54 +0200

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -19,21 +19,19 @@ const yes = bot.emojis.get("700713527576625205");
 const no = bot.emojis.get("700713478578634783");  
 let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
-let acceptchannel = bot.channels.find(x => x.name === 'accepted-applicants');
-
 geluktEmbed = new Discord.RichEmbed()
       .setColor("GREEN")
       .setTitle(`${yes} **Congratulations!**`)
       .setDescription(`<@${rUser.id}> Your application has been approved! You have taken your first step into the community. The next step is to wait for an interview to be announced/scheduled. We wish you good luck!`)
 
-acceptchannel.send(geluktEmbed);
+let acceptchannel = message.guild.channels.find(x => x.name === 'accepted-applicants');
+acceptchannel.send({geluktEmbed});
 
 let ModEmbed = new Discord.RichEmbed()
 .setTitle("**Moderation Command Used!**")
 .setTimestamp()
 .setColor("BLACK")
 .setDescription([
-    `**The administration command** !accept **has been used**`,
     ` `,
     `**Used On:** <@${rUser.id}>`,
     ` `,
