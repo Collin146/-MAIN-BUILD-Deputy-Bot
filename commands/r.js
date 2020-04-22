af6da84a867b6d728e2c1dfee9b76e1f09a11a42
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-22 13:35:03 +0200

    Update r.js

--- commands/r.js
+++ commands/r.js
@@ -4,6 +4,17 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+    function catchErr (err, message) {
+
+        let errchannel = bot.channels.find(x => x.name === 'errors');
+        const warningsign = bot.emojis.get("700843409526620180");
+        
+        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`r.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+        
+        }
+
+    try {
+
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
     if(args[0] === "help"){
         message.reply("Usage: !r");
@@ -27,6 +38,11 @@ let mentionrole = message.guild.roles.find(x => x.name === 'Member');
       ].join('\n'))
 
  message.delete().catch(O_o=>{});
+
+    } catch(err) {
+        catchErr(err)
+
+    }
 
 }
 
