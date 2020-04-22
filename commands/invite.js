0b794016101b0959c8ec112080136091552ea834
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-22 13:25:25 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,6 +3,17 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+    function catchErr (err, message) {
+
+        let errchannel = bot.channels.find(x => x.name === 'errors');
+        const warningsign = bot.emojis.get("700843409526620180");
+        
+        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`invite.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+        
+        }
+
+try {
+
 let mentionrole = message.guild.roles.find(x => x.name === 'Staff Team');
 let servernum = args.join(" ");
 const yes = bot.emojis.get("700713527576625205");
@@ -32,6 +43,11 @@ let doneembed = new Discord.RichEmbed()
 .setDescription("The Staff Team has been notified!")
 
 message.channel.send(doneembed);
+
+} catch(err) {
+    catchErr(err)
+
+}
 
 }
 
