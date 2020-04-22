a48bcc6ab1a4ffdf9ac5de29b2eb5099dcb8356c
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-22 13:53:23 +0200

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,6 +3,17 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+    function catchErr (err, message) {
+
+        let errchannel = bot.channels.find(x => x.name === 'errors');
+        const warningsign = bot.emojis.get("700843409526620180");
+        
+        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`suggest.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+        
+        }
+
+    try {
+
 const yes = bot.emojis.get("700713527576625205");
 const no = bot.emojis.get("700713478578634783"); 
 const maybe = bot.emojis.get("700713222566707203");
@@ -50,6 +61,11 @@ let doneembed = new Discord.RichEmbed()
 .setDescription("Your suggestion has been sent to the Staff Team!");
 
 message.channel.send(doneembed);
+
+    } catch(err) {
+        catchErr(err)
+        
+    }
 
 }
 
