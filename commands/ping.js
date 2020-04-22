5717255fa81b9b19b7b4ab1cbd0972db040d2c3f
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-22 13:31:50 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,9 +3,26 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+  function catchErr (err, message) {
+
+    let errchannel = bot.channels.find(x => x.name === 'errors');
+    const warningsign = bot.emojis.get("700843409526620180");
+    
+    errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`ping.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+    
+    }
+
+  try {
+
     const m = await message.channel.send("Ping?");
 
     m.edit(`Pong! **Latency is \`${m.createdTimestamp - message.createdTimestamp}ms\`. API Latency is \`${Math.round(bot.ping)}ms\`**`);
+
+  } catch(err) {
+    catchErr(err)
+
+  }
+
   }
 
 module.exports.help = {
