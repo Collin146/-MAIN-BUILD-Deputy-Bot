f750fe5599cdef5d9769d9b5422ce7a7e721511f
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-22 12:47:13 +0200

    Update briefing.js

--- commands/briefing.js
+++ commands/briefing.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const ms = require("ms");
 const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
@@ -9,88 +8,103 @@ module.exports.run = async (bot, message, args) => {
         let errchannel = bot.channels.find(x => x.name === 'errors');
         const warningsign = bot.emojis.get("700843409526620180");
         
-        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`briefing.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`civilian.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
         
         }
 
 try {
 
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+    if(!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "ADMINISTRATOR");
     if(args[0] === "help"){
-        message.reply("Usage: !briefing <time>");
+        message.reply("Usage: !bcso <user>");
         return;
     }
     
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You don't have permission to do that.");
     if(args[0] == "help"){
-        message.reply("Usage: !briefing <time>");
+        message.reply("Usage: !bcso <user>");
         return;
     }
 
-if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
-
 const yes = bot.emojis.get("700713527576625205");
-const no = bot.emojis.get("700713478578634783"); 
-let mentionrole = message.guild.roles.find(x => x.name === 'Member');
-//let mentionrole = message.guild.roles.find(`name`, `Member`);
-let mutetime = args[0];
+//const no = bot.emojis.get("700713478578634783"); 
+let tociv = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
 let errEmbed = new Discord.RichEmbed()
       .setColor("RED")
       .setTitle(`${no} **Error!**`)
-      .setDescription("You didn't specify a time!");
+      .setDescription("Was not able to find that user!");
 
-if(!mutetime) return message.channel.send(errEmbed);
+if(!tociv) return message.channel.send(errEmbed);
+let memberrole = message.guild.roles.find(x => x.name === 'Member');
+let civilianrole = message.guild.roles.find(x => x.name === 'Civilian');
+let civ1role = message.guild.roles.find(x => x.name === 'Civilian I');
+let nmrole = message.guild.roles.find(x => x.name === 'New Member');
 
-if(args[0] == "cancel"){
-
-    let dm2embed = new Discord.RichEmbed()
-    .setTitle(`${yes} **Done!**`)
-    .setColor("GREEN")
-    .setDescription(`The briefing room announcement has successfully been cancelled.`)
-    .setTimestamp();
-    
-     message.author.send(dm2embed);
-    
-     return stop;
-
-     function stop() {
-         if (mutetime) {
-             clearTimeout(mutetime);
-             mutetime = 0;
-         }
- }
- 
-    }
-
-setTimeout(function(){
-    message.channel.send([
-        `<@&${mentionrole.id}>`,
-        ` `,
-        "⚠ **Join Briefing Room Now!** ⚠"
-      ].join('\n'))
-}, ms(mutetime));
-
-let dmembed = new Discord.RichEmbed()
-.setTitle(`${yes} **Done!**`)
-.setColor("GREEN")
-.setDescription(`I will post an announcement in ${message.channel} for briefing room in ${mutetime}.`)
-.setTimestamp();
-try{
-    await message.author.send(dmembed);
-}catch(e){
-
-    let errEmbed2 = new Discord.RichEmbed()
+let errEmbed2 = new Discord.RichEmbed()
       .setColor("RED")
       .setTitle(`${no} **Error!**`)
-      .setDescription("Your DMs are locked. I cannot send you the confirmation message!");
+      .setDescription("You cannot use this command on an administrator.");
 
-    message.channel.send(errEmbed2);
+if(tociv.hasPermission("ADMINISTRATOR")) return message.channel.send(errEmbed2);
+//let memrrole = message.guild.roles.find(`name`, "ember");
+//let approle = message.guild.roles.find(`name`, "Applicant");
+//guildmember.setRoles(...)
+    
+let errEmbed3 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("The role \"Member\" doesn't exist.");
 
- }
- message.delete().catch(O_o=>{})
+   if(!memberrole) return message.channel.send(errEmbed3);
 
-} catch(err){
+let errEmbed4 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("The role \"Civilian\" doesn't exist.");
+
+   if(!civilianrole) return message.channel.send(errEmbed4);
+
+let errEmbed5 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("The role \"Probationary Civilian\" doesn't exist.");
+
+   if(!civ1role) return message.channel.send(errEmbed5);
+
+tociv.removeRole(nmrole.id);
+tociv.addRole(memberrole.id);
+tociv.addRole(civilianrole.id);
+tociv.addRole(civ1role.id);
+
+let ModEmbed = new Discord.RichEmbed()
+.setTitle("**Moderation Command Used!**")
+.setTimestamp()
+.setColor("BLACK")
+.setDescription([
+    `**The moderation command** !civilian **has been used**`,
+    ` `,
+    `**Used On:** <@${tociv.id}>`,
+    ` `,
+    `**Used In:** ${message.channel}`,
+    ` `,
+    `**Used By:** ${message.author.username}`
+  ].join('\n'))
+.setFooter(`Message ID: ${message.id} | Author ID: ${message.author.id}`);
+
+let modlogchannel = message.guild.channels.find(x => x.name === 'modlog');
+modlogchannel.send({embed: ModEmbed});
+
+
+geluktEmbed = new Discord.RichEmbed()
+      .setColor("GREEN")
+      .setTitle(`${yes} **Done!**`)
+      .setDescription(`The roles of <@${tociv.id}> have been updated!`)
+      .setFooter(`Mentioned User ID: ${tociv.id}`);
+
+      message.channel.send(geluktEmbed);
+
+} catch(err) {
     catchErr(err)
 
 }
@@ -98,5 +112,5 @@ try{
 }
 
 module.exports.help = {
-    name: "briefing"
+    name: "civilian"
 }
