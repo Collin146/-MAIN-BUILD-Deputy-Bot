2617c9b1c51bf52b343222f224043a62a00a5695
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-22 13:55:05 +0200

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -4,6 +4,17 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+    function catchErr (err, message) {
+
+        let errchannel = bot.channels.find(x => x.name === 'errors');
+        const warningsign = bot.emojis.get("700843409526620180");
+        
+        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`tempmute.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+        
+        }
+
+    try {
+
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
     if(args[0] === "help"){
         message.reply("Usage: !tempmute <user> <time>");
@@ -113,7 +124,11 @@ let ModEmbed = new Discord.RichEmbed()
 let modlogchannel = message.guild.channels.find(x => x.name === 'modlog');
 modlogchannel.send({embed: ModEmbed});
 
-//end of module
+    } catch(err) {
+        catchErr(err)
+        
+    }
+
 }
 
 module.exports.help = {
