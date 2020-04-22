05e9de0ee5fd707840e422dec727985252b5f7db
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-22 13:42:00 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,12 +2,27 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-//!say hi
-//hi
+    function catchErr (err, message) {
+
+        let errchannel = bot.channels.find(x => x.name === 'errors');
+        const warningsign = bot.emojis.get("700843409526620180");
+        
+        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`say.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+        
+        }
+
+try {
+
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have permission to do that.");
 let botmessage = args.join(" ");
 message.delete().catch();
 message.channel.send(botmessage);
+
+} catch(err) {
+    catchErr(err)
+
+}
+
 }
 
 module.exports.help = {
