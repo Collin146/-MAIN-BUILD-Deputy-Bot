199444abe0fc8aae128bcbffa2efd21897c4279c
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-22 12:39:49 +0200

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,7 +1,19 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let bicon = bot.user.displayAvatarURL;
+
+    function catchErr (err, message) {
+
+        let errchannel = bot.channels.find(x => x.name === 'errors');
+        const warningsign = bot.emojis.get("700843409526620180");
+        
+        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`botinfo.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+        
+        }
+
+try {
+
+    //let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
     .setTitle("**Bot Information**")
     .setColor("#ff6a00")
@@ -11,6 +23,11 @@ module.exports.run = async (bot, message, args) => {
     .addField("This bot was created and developed by:", "Collin A.#6966")
     return message.channel.send(botembed);
 
+
+} catch (err) {
+    catchErr(err)
+
+}
 
 }
 
