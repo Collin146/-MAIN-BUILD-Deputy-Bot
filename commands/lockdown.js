96c55d946a05ba6f215e7c86fdcb3756b3e5461f
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-22 13:29:55 +0200

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -3,16 +3,27 @@ const ms = require("ms");
 const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
+
+  function catchErr (err, message) {
+
+    let errchannel = bot.channels.find(x => x.name === 'errors');
+    const warningsign = bot.emojis.get("700843409526620180");
+    
+    errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`lockdown.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+    
+    }
+
+try {
     
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
     if(args[0] === "help"){
-        message.reply("Usage: !lockdown <length>");
+        message.reply("Usage: !lockdown <lock/unlock>");
         return;
     }
 
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
     if(args[0] == "help"){
-        message.reply("Usage: !lockdown <length>");
+        message.reply("Usage: !lockdown <lock/unlock>");
         return;
     }
     
@@ -55,6 +66,12 @@ module.exports.run = async (bot, message, args) => {
       });
     }
     }
+
+  } catch(err) {
+    catchErr(err)
+
+  }
+
 }
 
 module.exports.help = {
