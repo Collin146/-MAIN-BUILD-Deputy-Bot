f38d1d906b3eef8a0da9f1b2f79a51009ac52f23
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-22 13:20:57 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,8 +2,19 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-const yes = bot.emojis.get("700713527576625205");
-const no = bot.emojis.get("700713478578634783"); 
+    function catchErr (err, message) {
+
+        let errchannel = bot.channels.find(x => x.name === 'errors');
+        const warningsign = bot.emojis.get("700843409526620180");
+        
+        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`help.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+        
+        }
+
+    try {
+
+    const yes = bot.emojis.get("700713527576625205");
+    const no = bot.emojis.get("700713478578634783"); 
 
     geluktEmbed = new Discord.RichEmbed()
     .setColor("GREEN")
@@ -11,6 +22,11 @@ const no = bot.emojis.get("700713478578634783");
     .setDescription(`Here is the command list of this bot: \https://docs.google.com/document/d/10m06EtPOt64mJ86K2IFeXH5c5tVN-F2bZ9r8x4ikc5o/edit?usp=sharing\ `)
 
     message.channel.send(geluktEmbed);
+
+    } catch(err) {
+        catchErr(err)
+
+    }
 
 }
 
