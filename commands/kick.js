73b3c955fa50bee84cb48fb93bfaec0c15a511cc
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-22 13:27:48 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,6 +3,17 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+    function catchErr (err, message) {
+
+        let errchannel = bot.channels.find(x => x.name === 'errors');
+        const warningsign = bot.emojis.get("700843409526620180");
+        
+        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`kick.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+        
+        }
+
+    try {
+
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
     if(args[0] === "help"){
         message.reply("Usage: !kick <user> <reason>");
@@ -71,6 +82,11 @@ modlogchannel.send({embed: ModEmbed});
     .addField("Reason:", kReason || "None");
 
     kUser.send(dmembed);
+
+    } catch(err) {
+        catchErr(err)
+
+    }
 
 }
 
