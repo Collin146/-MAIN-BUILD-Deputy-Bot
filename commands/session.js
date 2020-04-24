b4fb4766da546b897cb37574d350eeaf1db30245
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-24 13:40:08 +0200

    Update session.js

--- commands/session.js
+++ commands/session.js
@@ -54,7 +54,7 @@ message.channel.send([
     `\`${day}\` **at** \`${time}\` **PM BST**`,
     ` `,
     "**Say yes to attend**",
-    `(If you say yes you are required to show up to the patrol. If you can't show up with a reason inform ${message.author} __before__ the patrol starts.)`
+    `(If you say yes you are required to show up to the patrol. If you can't show up with a reason inform ${message.author} __before__ the patrol starts).`
   ].join('\n'))
 
   message.delete().catch(O_o=>{});
