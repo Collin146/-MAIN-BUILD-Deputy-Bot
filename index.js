e1f1719d7bdb0e9529166b262aaf134b013c4ecf
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-04-29 15:11:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ try {
 //const image = rando_imgs[Math.floor(Math.random() * rando_imgs.length)];
 let memberTag = member.user.tag;
 
-let welcuser = member.guild.member(member) || await member.guild.fetchMember(member) || await bot.fetchUser(member)
+let welcuser = member.guild.member(member) || member.guild.fetchMember(member) || bot.fetchUser(member)
 
 const imagetouse = ("https://cdn.discordapp.com/attachments/461540254441144326/689179495000703063/TRANSP_WELCOME-cutout.png")
     
