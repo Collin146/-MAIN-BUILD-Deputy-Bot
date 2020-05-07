d950fe040cf5636ceb9d61de96f685d5e88e0855
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-05-07 16:28:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -857,7 +857,32 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
 
     } else if (oldUserChannel !== null && newUserChannel !== null) {
     
-    if(newUserChannel.id === Briefingroom.id || Queue.id || ten1.id || RA1.id || RA2.id || RA3.id || RTO.id || nine11.id || Traffic1.id || Traffic2.id || Traffic3.id || Traffic4.id || Scene1.id || Scene2.id || Scene3.id || Scene4.id || Civ1.id || Civ2.id || Civ3.id || Civ4.id || Civ5.id || Civ6.id || Civ7.id) return; 
+    //if(newUserChannel.id === Briefingroom.id, Queue.id, ten1.id, RA1.id, RA2.id, RA3.id, RTO.id, nine11.id, Traffic1.id, Traffic2.id, Traffic3.id, Traffic4.id, Scene1.id, Scene2.id, Scene3.id, Scene4.id, Civ1.id, Civ2.id, Civ3.id, Civ4.id, Civ5.id, Civ6.id, Civ7.id) return;
+
+    if(newUserChannel.id === Briefingroom.id) return;
+    if(newUserChannel.id === Queue.id) return;
+    if(newUserChannel.id === ten1.id) return;
+    if(newUserChannel.id === RA1.id) return;
+    if(newUserChannel.id === RA2.id) return;
+    if(newUserChannel.id === RA3.id) return;
+    if(newUserChannel.id === RTO.id) return;
+    if(newUserChannel.id === nine11.id) return;
+    if(newUserChannel.id === Traffic1.id) return;
+    if(newUserChannel.id === Traffic2.id) return;
+    if(newUserChannel.id === Traffic3.id) return;
+    if(newUserChannel.id === Traffic4.id) return;
+    if(newUserChannel.id === Scene1.id) return;
+    if(newUserChannel.id === Scene2.id) return;
+    if(newUserChannel.id === Scene3.id) return;
+    if(newUserChannel.id === Scene4.id) return;
+    if(newUserChannel.id === Civ1.id) return;
+    if(newUserChannel.id === Civ2.id) return;
+    if(newUserChannel.id === Civ3.id) return;
+    if(newUserChannel.id === Civ4.id) return;
+    if(newUserChannel.id === Civ5.id) return;
+    if(newUserChannel.id === Civ6.id) return;
+    if(newUserChannel.id === Civ7.id) return;
+
     newMember.removeRole(patrolrole.id);
 
     }
