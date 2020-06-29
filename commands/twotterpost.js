fb0fe208f6739006b0a9147483d15d8bcff239d6
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-06-29 16:36:01 +0200

    Update twotterpost.js

--- commands/twotterpost.js
+++ commands/twotterpost.js
@@ -1,24 +1,28 @@
 const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
 
-module.exports.run = async (bot, message, args) => { 
+module.exports.run = async (bot, message, args) => 
 
-
+const yes = client.emojis.get("700713527576625205");
+const no = client.emojis.get("700713478578634783"); 
 const twotter = bot.emojis.get("727159498686595072");
-//let messagecont = args.slice(1).join(" ");
-let messagecont = args.join(" ").slice(0);
+let postcontent = args.join(" ").slice(0);
 
-message.delete().catch();
+let errEmbed = new Discord.RichEmbed()
+.setColor("RED")
+.setTitle(`${no} **Error!**`)
+.setDescription(`You didn't provide a message!`);
 
-const postimage = ("https://media.discordapp.net/attachments/511913643923996683/727158652888154163/image1.png")
-    
-    let postembed = new Discord.RichEmbed()
-      .setTitle("**New Twotter Post**")
-      .setColor("#0173ce")
-      .setDescription(`${twotter} ${messagecont}`)
-      .setImage(`${postimage}`);
-      
-  message.channel.send({postembed});
+if(!postcontent) return message.channel.send(errEmbed);
+
+let postembed = new Discord.RichEmbed()
+.setTitle("**New Twotter Post!**")
+.setColor("0173ce")
+.setDescription(`${postcontent}`);
+
+
+message.channel.send(postembed);
+
 
 }
 
