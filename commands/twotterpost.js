c91fdc01c14b1a044f0e2ec5d5133dfd387bb45a
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-06-29 16:23:40 +0200

    Update twotterpost.js

--- commands/twotterpost.js
+++ commands/twotterpost.js
@@ -15,7 +15,7 @@ const postimage = ("https://media.discordapp.net/attachments/511913643923996683/
     let postembed = new Discord.RichEmbed()
       .setTitle("**New Twotter Post**")
       .setColor("#0173ce")
-      .setDescription(`${twotter} ${messagecont}`)
+      .setDescription(`${twotter} > ${messagecont}`)
       .setImage(`${postimage}`);
       
   message.channel.send({postembed});
