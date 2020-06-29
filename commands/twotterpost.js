8f2e5a08680e144635d003b8ed9d1d1a0b4139c8
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-06-29 16:42:31 +0200

    Update twotterpost.js

--- commands/twotterpost.js
+++ commands/twotterpost.js
@@ -15,10 +15,13 @@ let errEmbed = new Discord.RichEmbed()
 
 if(!postcontent) return message.channel.send(errEmbed);
 
+const postimage = ("https://media.discordapp.net/attachments/511913643923996683/727158652888154163/image1.png")
+
 let postembed = new Discord.RichEmbed()
 .setTitle("**New Twotter Post!**")
 .setColor("0173ce")
-.setDescription(`${postcontent}`);
+.setDescription(`${twotter} ${postcontent}`)
+.setImage(`${postimage}`);
 
 
 message.channel.send(postembed);
