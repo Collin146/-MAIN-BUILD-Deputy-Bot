a944a70d4eefd0efeb4da72d5be225c78bc44b14
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-06-29 16:12:24 +0200

    Update twotter.js

--- commands/twotter.js
+++ commands/twotter.js
@@ -3,6 +3,9 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
+
+const twotter = bot.emojis.get("727159498686595072");
+
 if(args[0] === "post"){
 
 let messagecont = args.slice(1).join(" ");
@@ -14,7 +17,7 @@ const postimage = ("https://media.discordapp.net/attachments/511913643923996683/
     let postembed = new Discord.RichEmbed()
       .setTitle("**New Twotter Post**")
       .setColor("#0173ce")
-      .setDescription(`${messagecont}`)
+      .setDescription(`${twotter} ${messagecont}`)
       .setImage(`${postimage}`);
       
   message.channel.send({postembed});
