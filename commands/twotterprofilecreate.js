61c652f363c8ed1e2af7357d914f829501f8ccb6
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-06-30 17:24:22 +0200

    Update twotterprofilecreate.js

--- commands/twotterprofilecreate.js
+++ commands/twotterprofilecreate.js
@@ -1,33 +1,12 @@
- const Discord = require("discord.js");
+  const Discord = require("discord.js");
  const fs = require("fs");
  const errors = require("../utils/errors.js");
- 
+
  module.exports.run = async (bot, message, args) => {
 
  const yes = bot.emojis.get("700713527576625205");
- const no = bot.emojis.get("700713478578634783"); 
+ const no = bot.emojis.get("700713478578634783l"); 
  const twotter = bot.emojis.get("727159498686595072");
-
-function jsonReader(filePath, cb) {
-  fs.readFile(filePath, (err, fileData) => {
-    if (err) {
-      return cb && cb(err);
-    }
-    try {
-      const object = JSON.parse(fileData);
-      return cb && cb(null, object);
-    } catch (err) {
-      return cb && cb(err);
-    }
-  });
-}
-jsonReader("./profiles.json", (err, profiles) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(profiles.civfn); // => "Infinity Loop Drive"
-});
 
 const profile = { 
     username: message.author.username,
