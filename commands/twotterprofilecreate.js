769c436d8e3594412f78b9ddd8d50f19d3411ef9
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-06-30 16:59:00 +0200

    Update twotterprofilecreate.js

--- commands/twotterprofilecreate.js
+++ commands/twotterprofilecreate.js
@@ -1,6 +1,7 @@
- const Discord = require("discord.js");
+const Discord = require("discord.js");
  const fs = require("fs");
  const errors = require("../utils/errors.js");
+ const profiles = require("./profiles.json");
 
  module.exports.run = async (bot, message, args) => {
 
@@ -8,17 +9,23 @@
  const no = bot.emojis.get("700713478578634783"); 
  const twotter = bot.emojis.get("727159498686595072");
 
-let profile = { 
-    username: message.author.id,
+const profile = { 
+    username: message.author.username,
     civfn: args[0],
     civln: args[1],
     age: args[2],
-    bio: args.join(" ").slice(2)
-};
- 
-let data = JSON.stringify(profile);
-fs.writeFileSync('./profiles.json', data);
+    bio: args.join(" ").slice(2),
+}
 
+const jsonString = JSON.stringify(profile)fs.writeFile('./profiles.json', jsonString, err => {
+    if (err) {
+        console.log('Error writing file', err)
+    } else {
+        console.log('Successfully wrote file')
+    }
+})
+
+ 
  }
 
  module.exports.help = {
