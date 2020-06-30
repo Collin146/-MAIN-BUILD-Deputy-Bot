b8e6965f553258072bcac74304fc122bb5947f61
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-06-30 17:30:11 +0200

    Update twotterprofilecreate.js

--- commands/twotterprofilecreate.js
+++ commands/twotterprofilecreate.js
@@ -1,4 +1,4 @@
-  const Discord = require("discord.js");
+   const Discord = require("discord.js");
  const fs = require("fs");
  const errors = require("../utils/errors.js");
 
@@ -7,6 +7,20 @@
  const yes = bot.emojis.get("700713527576625205");
  const no = bot.emojis.get("700713478578634783l"); 
  const twotter = bot.emojis.get("727159498686595072");
+ 
+ function jsonReader(filePath, cb) {
+  fs.readFile(filePath, (err, fileData) => {
+    if (err) {
+      return cb && cb(err);
+    }
+    try {
+      const object = JSON.parse(fileData);
+      return cb && cb(null, object);
+    } catch (err) {
+      return cb && cb(err);
+    }
+  });
+}
 
 const profile = { 
     username: message.author.username,
@@ -16,16 +30,16 @@ const profile = {
     bio: args.join(" ").slice(2),
 }
 
-const jsonString = JSON.stringify(profile)
-
-fs.writeFileSync('./profiles.json', jsonString, err => {
-    if (err) {
-        console.log('Error writing file', err)
-    } else {
-        console.log('Successfully wrote file')
-    }
-})
-
+jsonReader("./profiles.json", (err, profile) => {
+  if (err) {
+    console.log("Error reading file:", err);
+    return;
+  }
+  
+  fs.writeFile("./profiles.json", JSON.stringify(profile), err => {
+    if (err) console.log("Error writing file:", err);
+  });
+});
  
  }
 
