5c5673fb3165dda78e140ca9e1035b6595373a89
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-01 14:06:38 +0200

    Update bcso.js

--- commands/bcso.js
+++ commands/bcso.js
@@ -78,16 +78,13 @@ tobcso.addRole(bcsorole.id);
 tobcso.addRole(probrole.id);
 
 let ModEmbed = new Discord.RichEmbed()
-.setTitle("**Moderation Command Used!**")
+.setTitle("**Command Used!**")
 .setTimestamp()
 .setColor("BLACK")
 .setDescription([
-    `**The moderation command** !bcso **has been used**`,
-    ` `,
+    `**Command:** !bcso`,
     `**Used On:** <@${tobcso.id}>`,
-    ` `,
     `**Used In:** ${message.channel}`,
-    ` `,
     `**Used By:** ${message.author.username}`
   ].join('\n'))
 .setFooter(`Message ID: ${message.id} | Author ID: ${message.author.id}`);
