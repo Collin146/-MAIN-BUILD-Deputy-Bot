8ee1315fa3a9cdda353aee7f126dcb6e9d03fd11
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-01 11:36:03 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -51,59 +51,6 @@ try {
 //         let warnchannel = message.guild.channels.find(`name`, "modlog");
 //         if(!warnchannel) return message.reply("Couldn't find channel");
         
-const Discord = require("discord.js");
-const errors = require("../utils/errors.js");
-
-module.exports.run = async (bot, message, args) => { 
-
-    function catchErr (err, message) {
-
-        let errchannel = bot.channels.find(x => x.name === 'errors');
-        const warningsign = bot.emojis.get("700843409526620180");
-        
-        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`clear.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
-        
-        }
-
-try {
-
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
-    if(args[0] === "help"){
-        message.reply("Usage: !clear <amount>");
-        return;
-    }
-
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
-    if(args[0] == "help"){
-        message.reply("Usage: !clear <amount>");
-        return;
-    }
-    
-//     if (args[0] > 100) {
-
-//         let args100 = args[0] - 100
-//     message.channel.bulkDelete(100)
-//     message.channel.bulkDelete(args100).then(() => {
-//         message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
-
-//         let ModEmbed = new Discord.RichEmbed()
-//         .setTitle("**Moderation Command Used!**")
-//         .setTimestamp()
-//         .setColor("BLACK")
-//         .setDescription([
-//             `**The moderation command** !clear **has been used**`,
-//             ` `,
-//             `**Cleared Amount:** ${args}`,
-//             ` `,
-//             `**Used In:** ${message.channel}`,
-//             ` `,
-//             `**Used By:** ${message.author.username}`
-//           ].join('\n'))
-//         .setFooter(`Message ID: ${message.id} | Author ID: ${message.author.id}`);
-        
-//         let warnchannel = message.guild.channels.find(`name`, "modlog");
-//         if(!warnchannel) return message.reply("Couldn't find channel");
-        
 //         warnchannel.send(ModEmbed);
 
 // return;
