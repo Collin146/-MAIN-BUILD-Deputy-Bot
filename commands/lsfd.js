51f38fe9d3403212a1aa1a52c2ecf7e18a86eb11
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-01 14:44:27 +0200

    Update lsfd.js

--- commands/lsfd.js
+++ commands/lsfd.js
@@ -78,16 +78,13 @@ tobcso.addRole(lsfdrole.id);
 tobcso.addRole(probrole.id);
 
 let ModEmbed = new Discord.RichEmbed()
-.setTitle("**Moderation Command Used!**")
+.setTitle("**Command Used!**")
 .setTimestamp()
 .setColor("BLACK")
 .setDescription([
-    `**The moderation command** !lsfd **has been used**`,
-    ` `,
+    `**Command:** !lsfd`,
     `**Used On:** <@${tobcso.id}>`,
-    ` `,
     `**Used In:** ${message.channel}`,
-    ` `,
     `**Used By:** ${message.author.username}`
   ].join('\n'))
 .setFooter(`Message ID: ${message.id} | Author ID: ${message.author.id}`);
