7063350e76214a2b56d2f9582a5bae8dbdbc7c68
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-05 15:37:21 +0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -8,13 +8,3 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
     name: "userinfo"
 }
-const Discord = require("discord.js");
-const errors = require("../utils/errors.js");
-
-module.exports.run = async (bot, message, args) => {
-
-}
-
-module.exports.help = {
-    name: "userinfo"
-}
