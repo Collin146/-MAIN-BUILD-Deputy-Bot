6403aa67d72fd4e31920d91ae95372981e36c1c6
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-06 18:05:21 +0200

    Added patrol alert to alert.js

--- commands/alert.js
+++ commands/alert.js
@@ -2,20 +2,10 @@ const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args, channel) => {
-
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
-    if(args[0] === "help"){
-        message.reply("Usage: !alert <alert type> <info>");
-        return;
-    }
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
-    if(args[0] == "help"){
-        message.reply("Usage: !alert <alert type> <info>");
-        return;
-    }
     
 const yes = bot.emojis.get("700713527576625205");
 const no = bot.emojis.get("700713478578634783"); 
+const warningsign = bot.emojis.get("729725849343098900");
 const firebot = bot.emojis.get("729718092560728095");
 const lightbar = bot.emojis.get("729717051488206918");
 
@@ -28,6 +18,7 @@ let errEmbed = new Discord.RichEmbed()
     
     let mentionrole = message.guild.roles.find(x => x.name === 'On Patrol');
     const provinfo = args.join(" ").slice(12);
+    if (!provinfo) return message.channel.send(errEmbed);
     
     let errEmbed2 = new Discord.RichEmbed()
       .setColor("RED")
@@ -42,6 +33,8 @@ let errEmbed = new Discord.RichEmbed()
         `A silent alarm has been received from ${provinfo}. Please notify dispatch if active!`,
       ].join('\n'))
         
+        message.delete().catch(O_o=>{});
+        
         return;
             }
     
@@ -49,6 +42,7 @@ let errEmbed = new Discord.RichEmbed()
     
     let mentionrole = message.guild.roles.find(x => x.name === 'On Patrol');
     const provinfo2 = args.join(" ").slice(10);
+    if (!provinfo2) return message.channel.send(errEmbed);
     
     let errEmbed2 = new Discord.RichEmbed()
       .setColor("RED")
@@ -63,8 +57,55 @@ let errEmbed = new Discord.RichEmbed()
         `A fire alarm has been received from ${provinfo2}. Please notify dispatch if active!`,
       ].join('\n'))
         
+        message.delete().catch(O_o=>{});
+        
         return;
             }
+    
+    if(args[0] === "patrol"){
+    
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+    if(args[0] === "help"){
+        message.reply("Usage: !alert <alert type> <info>");
+        return;
+    }
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+    if(args[0] == "help"){
+        message.reply("Usage: !alert <alert type> <info>");
+        return;
+    }
+    
+    let errEmbed3 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("Please provide a message!");
+    
+    let mentionrole = message.guild.roles.find(x => x.name === 'On Patrol');
+    const provinfo3 = args.join(" ").slice(7);
+    if (!provinfo3) return message.channel.send(errEmbed3);
+    
+    let errEmbed2 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("The role \"On Patrol\" doesn't exist.");
+    
+    if (!mentionrole) return message.channel.send(errEmbed2);
+        message.channel.send([
+        `<@&${mentionrole.id}>`,
+        ` `,
+        `${warningsign} **Administrative Message!** ${warningsign}`,
+        `${provinfo3}`,
+      ].join('\n'))
+        
+        message.delete().catch(O_o=>{});
+        
+        return;
+            }
+    
+    let errEmbed4 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("Please provide a type of alert! This can either be \"silentalarm\", \"firealarm\", or \"patrol\".");
     
     }
 
