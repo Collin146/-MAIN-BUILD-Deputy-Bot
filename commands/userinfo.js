43fc21d1e54a89ab6559d3157a4e5a14d5acc22b
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-06 09:56:28 +0200

    Edit iduser fetch mention instantly in userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot, message, args) => {
 
 const iuser = message.guild.member(message.mentions.users.first()) || await bot.fetchUser(args[0]);
 
-const iduser = message.mentions.members.first(iuser)
+const iduser = message.mentions.members.first()
 
 const yes = bot.emojis.get("700713527576625205");
 const no = bot.emojis.get("700713478578634783"); 
@@ -45,7 +45,7 @@ let userembed = new Discord.RichEmbed()
       .setThumbnail(uicon)
       .setDescription([
        `**General Information**`,
-       `Username: ${iuser.username}`,
+       `Username: ${iduser.username}`,
        ` `,
        `Nickname: ${iuser.nickname || "None"}`,
        ` `,
