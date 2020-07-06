004bca0b61aef18621d94cbf035c1992216fdeb2
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-06 17:32:58 +0200

    Imported alert.js from workspace

--- commands/alert.js
+++ commands/alert.js
@@ -14,6 +14,57 @@ module.exports.run = async (bot, message, args, channel) => {
         return;
     }
     
+const yes = bot.emojis.get("700713527576625205");
+const no = bot.emojis.get("700713478578634783"); 
+const firebot = bot.emojis.get("729718092560728095");
+const lightbar = bot.emojis.get("729717051488206918");
+
+let errEmbed = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("Please provide a postal code & street name!");
+    
+    if(args[0] === "silentalarm"){
+    
+    let mentionrole = message.guild.roles.find(x => x.name === 'On Patrol');
+    const provinfo = args.join(" ").slice(1);
+    
+    let errEmbed2 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("The role \"On Patrol\" doesn't exist.");
+    
+    if (!mentionrole) return message.channel.send(errEmbed2);
+        message.channel.send([
+        `<@&${mentionrole.id}>`,
+        ` `,
+        `${lightbar} **Silent Alarm!** ${lightbar}`
+        `A silent alarm has been received from ${provinfo}. Please notify dispatch if active!`,
+      ].join('\n'))
+        
+        return;
+            }
+    
+    if(args[0] === "firealarm"){
+    
+    let mentionrole = message.guild.roles.find(x => x.name === 'On Patrol');
+    const provinfo2 = args.join(" ").slice(1);
+    
+    let errEmbed2 = new Discord.RichEmbed()
+      .setColor("RED")
+      .setTitle(`${no} **Error!**`)
+      .setDescription("The role \"On Patrol\" doesn't exist.");
+    
+    if (!mentionrole) return message.channel.send(errEmbed2);
+        message.channel.send([
+        `<@&${mentionrole.id}>`,
+        ` `,
+        `${firebot} **Fire Alarm!** ${firebot}`
+        `A fire alarm has been received from ${provinfo2}. Please notify dispatch if active!`,
+      ].join('\n'))
+        
+        return;
+            }
     
     }
 
