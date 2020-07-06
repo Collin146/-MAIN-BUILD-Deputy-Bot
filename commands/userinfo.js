5e0c27f8ab3d45d15ce0efbf5d6351ae6cf7eb23
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-06 10:29:18 +0200

    Removed function to send err in console log

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -3,15 +3,6 @@ const errors = require("../utils/errors.js");
 const moment = require("moment");
 
 module.exports.run = async (bot, message, args) => { 
-
-function catchErr (err, message) {
-
-        let errchannel = bot.channels.find(x => x.name === 'errors');
-        const warningsign = bot.emojis.get("700843409526620180");
-        
-        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`unmute.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
-        
-        }
         
         try {
 
