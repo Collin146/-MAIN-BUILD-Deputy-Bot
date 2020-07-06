d06a6c926369240932de85c95cbda50c2338633e
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-06 11:26:47 +0200

    Fixed undefined issue in userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -20,7 +20,7 @@ module.exports.run = async (bot, message, args) => {
 
 const iuser = message.guild.member(message.mentions.users.first()) || await bot.fetchUser(args[0]);
 
-const iduser = message.mentions.users.first()
+const iduser = message.mentions.users.first() || await bot.fetchUser(args[0]);
 
 const yes = bot.emojis.get("700713527576625205");
 const no = bot.emojis.get("700713478578634783"); 
