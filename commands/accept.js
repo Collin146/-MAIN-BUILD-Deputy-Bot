dc6cbf18dc69b843a78093c74246de1848b5e102
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-11-12 02:40:04 +0100

    Pushed accept.js from QA to MAIN

--- commands/accept.js
+++ commands/accept.js
@@ -63,7 +63,10 @@ let acceptembed = new Discord.RichEmbed()
           `**From:** ${message.guild.name}`,
           `**Date & Time:** ${moment.utc(message.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`,
           `**Status:** Approved`,
-          `**Message:** Your application has been approved! You have taken your first step into the community. The next step is to wait for an interview to be announced/scheduled in <#644273618011815946>. We wish you good luck! Please keep in mind that you will have a total of 30 days to attend an interview. If you fail to do so within those 30 days, your approved for interview status will unfortunately be removed and you will be forced to reapply if you want to be eligible for interviews again. To see how many days you have left, please refer to the following document: https://bit.ly/3kxyyr5 (note: it may take several hours before you get added to the document).`,
+          `**Message:** Your application has been approved! You have taken your first step into the community. The next step is to wait for an interview to be announced/scheduled in <#644273618011815946>. We wish you good luck!`,
+          ` `,
+          `Please keep in mind that you will have a total of 30 days to attend an interview. If you fail to do so within those 30 days, your approved for interview status will unfortunately be removed and you will be forced to reapply if you want to be eligible for interviews again. To see how many days you have left, please refer to the following document: https://bit.ly/3kxyyr5`,
+          `(Please note that it may take several hours until you get added to this document).`
         ].join('\n'))
     
 rUser.send(acceptembed);
