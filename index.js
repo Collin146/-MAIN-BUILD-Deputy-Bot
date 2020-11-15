f03e33cdefc42e7a2ef5c0441401b1ec84da9a00
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-11-15 02:05:32 +0100

    Pushed feedbackForm from QA to MAIN

--- index.js
+++ index.js
@@ -221,6 +221,51 @@ if (member.guild.id === "644227663829139466") {
     leftchannel.send(`**${memberTag}** has left the server.`);
 
 }
+
+if (member.guild.id === "604420918634086411") {
+
+    const yes = bot.emojis.get("700713527576625205");
+    const no = bot.emojis.get("700713478578634783");
+    const gno = bot.emojis.get("759495234928902154");
+    
+    const drp1 = bot.emojis.get("759125897953017857");
+    const drp2 = bot.emojis.get("759125936586883072");
+    const drp3 = bot.emojis.get("759125984967393330");
+    const drp4 = bot.emojis.get("759126011265941506");
+    const drp5 = bot.emojis.get("759126035215810592");
+    const drp6 = bot.emojis.get("759126060355813376");
+    const drp7 = bot.emojis.get("759126083781394444");
+    
+        let welcomedmEmbed = new Discord.RichEmbed()
+    .setTitle(`😕 **Sorry to see you leave!**`)
+    .setTimestamp()
+    .setColor("#00f4ef")
+    .setDescription([
+        `Hey there, we find very it unfortunate to see you leave the Deputy Roleplay Interview server. We at Deputy Roleplay always aim to improve and are open to any feedback!`,
+        ` `,
+        "**We would appreciate it a lot if you told us why you decided to leave. All you have to do is click the appropriate reaction signifying the reason why you left.**",
+        ` `,
+        `${drp1} - The application process seemed too extensive/complicated.`,
+        `${drp2} - I did not know how to apply/join the community.`,
+        `${drp3} - The community was not for the platform(s) I own.`,
+        `${drp4} - The application form was too hard for me.`,
+        `${drp5} - The department I was interested in was unavailable/closed.`,
+        `${drp6} - Other, (please click the reaction and message your reason here).`,
+        ` `,
+        `Please note that this message is automated and I am unable to reply to any questions you have.`,
+        ` `,
+        "Want to join back? [Click here](https://discord.gg/hR3rWWw)"
+      ].join('\n'))
+    
+      const sentMessage =  await member.send(welcomedmEmbed);
+      await sentMessage.react(drp1.id);
+      await sentMessage.react(drp2.id);
+      await sentMessage.react(drp3.id);
+      await sentMessage.react(drp4.id);
+      await sentMessage.react(drp5.id);
+      await sentMessage.react(drp6.id);
+
+}
     
 } catch (err) {
     catchErr(err);
@@ -1163,6 +1208,156 @@ modlogchannel.send({embed: modlogspamEmbed2});
 
 });
 
+bot.on('messageReactionAdd', async (messageReaction, user) => {
+
+    try {
+
+    if (user.bot) return;
+    
+    if (messageReaction.message.channel.type === 'dm') {
+
+        const yes = bot.emojis.get("700713527576625205");
+        const no = bot.emojis.get("700713478578634783");
+        const gno = bot.emojis.get("759495234928902154");
+        const drp1 = bot.emojis.get("759125897953017857");
+        const drp2 = bot.emojis.get("759125936586883072");
+        const drp3 = bot.emojis.get("759125984967393330");
+        const drp4 = bot.emojis.get("759126011265941506");
+        const drp5 = bot.emojis.get("759126035215810592");
+        const drp6 = bot.emojis.get("759126060355813376");
+        const drp7 = bot.emojis.get("759126083781394444");
+
+                    if (messageReaction.emoji.id === drp1.id) {
+
+                        let mainguild1 = bot.guilds.get('644254160019128320')
+            
+                        let feedbackEmbed1 = new Discord.RichEmbed()
+                        .setTitle("**Feedback Received!**")
+                        .setTimestamp()
+                        .setColor("BLACK")
+                        .setDescription([
+                            `**User:** ${user}, ${user.tag}`,
+                            `**User ID:** ${user.id}`,
+                            `**Reason For Leaving:** The application process seemed too extensive/complicated.`,
+                          ].join('\n'))
+                        
+                        let feedbackChannel = mainguild1.channels.find(x => x.name === 'left-members-feedback');
+                        feedbackChannel.send({embed: feedbackEmbed1}).then(user.send("Thank you for providing your feedback!"));
+ 
+                    }
+
+                    if (messageReaction.emoji.id === drp2.id) {
+
+                        let mainguild1 = bot.guilds.get('644254160019128320')
+                        
+                        let feedbackEmbed2 = new Discord.RichEmbed()
+                        .setTitle("**Feedback Received!**")
+                        .setTimestamp()
+                        .setColor("BLACK")
+                        .setDescription([
+                            `**User:** ${user}, ${user.tag}`,
+                            `**User ID:** ${user.id}`,
+                            `**Reason For Leaving:** I did not know how to apply/join the community.`,
+                        ].join('\n'))
+                        
+                        let feedbackChannel = mainguild1.channels.find(x => x.name === 'left-members-feedback');
+                        feedbackChannel.send({embed: feedbackEmbed2}).then(user.send("Thank you for providing your feedback!"));
+ 
+                    }
+
+                    if (messageReaction.emoji.id === drp3.id) {
+
+                        let mainguild1 = bot.guilds.get('644254160019128320')
+                        
+                        let feedbackEmbed3 = new Discord.RichEmbed()
+                        .setTitle("**Feedback Received!**")
+                        .setTimestamp()
+                        .setColor("BLACK")
+                        .setDescription([
+                            `**User:** ${user}, ${user.tag}`,
+                            `**User ID:** ${user.id}`,
+                            `**Reason For Leaving:** The community was not for the platform(s) I own.`,
+                        ].join('\n'))
+                        
+                        let feedbackChannel = mainguild1.channels.find(x => x.name === 'left-members-feedback');
+                        feedbackChannel.send({embed: feedbackEmbed3}).then(user.send("Thank you for providing your feedback!"));
+ 
+                    }
+
+                    if (messageReaction.emoji.id === drp4.id) {
+
+                        let mainguild1 = bot.guilds.get('644254160019128320')
+                        
+                        let feedbackEmbed4 = new Discord.RichEmbed()
+                        .setTitle("**Feedback Received!**")
+                        .setTimestamp()
+                        .setColor("BLACK")
+                        .setDescription([
+                            `**User:** ${user}, ${user.tag}`,
+                            `**User ID:** ${user.id}`,
+                            `**Reason For Leaving:** The application form was too hard for me.`,
+                        ].join('\n'))
+                        
+                        let feedbackChannel = mainguild1.channels.find(x => x.name === 'left-members-feedback');
+                        feedbackChannel.send({embed: feedbackEmbed4}).then(user.send("Thank you for providing your feedback!"));
+                        
+                    }
+
+                    if (messageReaction.emoji.id === drp5.id) {
+
+                        let mainguild1 = bot.guilds.get('644254160019128320')
+                        
+                        let feedbackEmbed5 = new Discord.RichEmbed()
+                        .setTitle("**Feedback Received!**")
+                        .setTimestamp()
+                        .setColor("BLACK")
+                        .setDescription([
+                            `**User:** ${user}, ${user.tag}`,
+                            `**User ID:** ${user.id}`,
+                            `**Reason For Leaving:** The department I was interested in was unavailable/closed.`,
+                        ].join('\n'))
+                        
+                        let feedbackChannel = mainguild1.channels.find(x => x.name === 'left-members-feedback');
+                        feedbackChannel.send({embed: feedbackEmbed5}).then(user.send("Thank you for providing your feedback!"));
+ 
+                    }
+
+                    if (messageReaction.emoji.id === drp6.id) {
+
+                        user.send("Please provide your feedback.")
+
+                        const filter = m => m.author.id === user.id;
+                
+                        user.dmChannel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
+                        .then((collected) => {
+
+                            let mainguild1 = bot.guilds.get('644254160019128320')
+                        
+                            let feedbackEmbed6 = new Discord.RichEmbed()
+                            .setTitle("**Feedback Received!**")
+                            .setTimestamp()
+                            .setColor("BLACK")
+                            .setDescription([
+                                `**User:** ${user}, ${user.tag}`,
+                                `**User ID:** ${user.id}`,
+                                `**Reason For Leaving (Other):** ${(collected.first().content) || "None"}`,
+                            ].join('\n'))
+                            
+                            let feedbackChannel = mainguild1.channels.find(x => x.name === 'left-members-feedback');
+                            feedbackChannel.send({embed: feedbackEmbed6}).then(user.send("Thank you for providing your feedback!"));
+
+                        });
+ 
+                    }
+
+    }
+
+
+} catch (err) {
+    catchErr(err);
+} 
+
+});
 
 bot.on("ready", async () => {
 
@@ -2017,6 +2212,11 @@ bot.on('message', message => {
 
       if (message.author.bot) return;
 
+      message.channel.fetchMessages({ limit: 2 }).then(fetchedMessages => {
+        const messagesToCheck = fetchedMessages.some(msg => (msg.content.includes('feedback.')));
+
+        if (!messagesToCheck) {
+
       message.author.send("I cannot reply to DM's. If you require support, please reach out to a staff member in any of the Deputy Roleplay servers.");
 
       let staffguild2 = bot.guilds.get('644254160019128320');
@@ -2037,6 +2237,10 @@ bot.on('message', message => {
       fetchchannelstaff.send({embed: dmmodlogembed});
 
       return;
+    }
+
+    });
+
     }
 
   });
