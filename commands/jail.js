4c5e68a5fdf051972eb7182e0a6ec05707c180c5
Collin146 <47307903+Collin146@users.noreply.github.com>
2021-02-14 18:53:12 +0100

    Updated jail.js

--- commands/jail.js
+++ commands/jail.js
@@ -15,90 +15,161 @@ module.exports.run = async (bot, message, args) => {
 
     try {
 
-const no = bot.emojis.get("700713478578634783"); 
-
-       let errEmbed = new Discord.RichEmbed()
-      .setColor("RED")
-      .setTitle(`${no} **Error!**`)
-      .setDescription(`Only LEO have permission to use this command!`)
-
-    guildAuthor = message.guild.members.get(message.author.id);
-
-    if(!guildAuthor.roles.has("644229497062948864")) return message.channel.send(errEmbed);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
     if(args[0] === "help"){
-        message.reply("Usage: !jail <user> <time> <reason>");
+        message.reply("Usage: !tempmute <user> <time>");
         return;
     }
     
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+    if(args[0] == "help"){
+        message.reply("Usage: !tempmute <user> <time length>");
+        return;
+    }
+
+//!tempmute @user 1s/m/h/d
+
 const yes = bot.emojis.get("700713527576625205");
-const warningsign = bot.emojis.get("729725849343098900");
-let tojail = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-let jailReason = args.slice(2).join(" ");
+const no = bot.emojis.get("700713478578634783"); 
+let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+let errEmbed = new Discord.RichEmbed()
+.setColor("RED")
+.setTitle(`${no} **Error!**`)
+.setDescription(`Was not able to find that user!`);
+
+if(!tomute) return message.channel.send(errEmbed);
 
 let errEmbed2 = new Discord.RichEmbed()
 .setColor("RED")
 .setTitle(`${no} **Error!**`)
-.setDescription(`You didn't specify the user!`);
+.setDescription(`You cannot mute a Moderator or higher.`);
 
-if (!tojail) return message.channel.send(errEmbed2);
+if(tomute.hasPermission("ADMINISTRATOR")) return message.channel.send(errEmbed2);
+let muterole = message.guild.roles.find(x => x.name === 'Muted');
+let memberrole = message.guild.roles.find(x => x.name === 'Member');
+let approle = message.guild.roles.find(x => x.name === 'Applicant');
+let recrole = message.guild.roles.find(x => x.name === 'Recruit');
+//start of create role
+if (!muterole){
+    try{
+        muterole = await message.guild.createRole({
+            name: "Muted",
+            color: "#000000",
+            permissions: []
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+            await channel.overwritePermissions(muterole, {
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            });
+        });
 
-let jailtime = args[1];
+    }catch(e){
+        console.log(e.stack);
+    }
+}
+//end of create role
+let mutetime = args[1];
 
 let errEmbed3 = new Discord.RichEmbed()
 .setColor("RED")
 .setTitle(`${no} **Error!**`)
 .setDescription(`You didn't specify a time!`);
 
-if(!jailtime) return message.channel.send(errEmbed3);
+if(!mutetime) return message.channel.send(errEmbed3);
 
 try {
 
-await(tojail.setVoiceChannel('807677661748920350'));
+await(tomute.addRole(muterole.id));
+await(tomute.removeRole(memberrole.id));
 
-} catch(err) {  
+} catch(err) {
 }
 
-message.delete().catch(_O_o=>{})
+try {
+
+await(tomute.addRole(muterole.id));
+await(tomute.removeRole(approle.id));
+
+} catch(err) {
+}
+
+try {
+
+await(tomute.addRole(muterole.id));
+await(tomute.removeRole(recrole.id));
+    
+} catch(err) {  
+}
 
 geluktEmbed = new Discord.RichEmbed()
       .setColor("GREEN")
       .setTitle(`${yes} **Done!**`)
-      .setDescription(`<@${tojail.id}> has been sentenced for ${ms(ms(jailtime))}.`)
-      .setFooter(`Mentioned User ID: ${tojail.id}`);
+      .setDescription(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}.`)
+      .setFooter(`Mentioned User ID: ${tomute.id}`);
 
-message.author.send(geluktEmbed);
+message.channel.send(geluktEmbed);
 
-jailEmbed = new Discord.RichEmbed()
-      .setColor("BLUE")
-      .setDescription(`**Judge:** <@${tojail.id}> has been sentenced for ${ms(ms(jailtime))} for ${jailReason}.`)
-
-let rpchannel = message.guild.channels.find(x => x.name === 'rp-actions');
-rpchannel.send({embed: jailEmbed});
-
-geluktEmbed3 = new Discord.RichEmbed()
-.setColor("RED")
-.setTitle(`${warningsign} **Jailed!**`)
-.setDescription(`You have been sentenced for ${ms(ms(jailtime))} for ${jailReason}. Due to this, you have been moved to the Penitentiary voice channel. Please stay there until you receive another message stating you have served your time.`)
-
-tojail.send(geluktEmbed3);
+if(!message.member.roles.find(r => r.name === "Muted"))
     
 setTimeout(function(){
 
+tomute.removeRole(muterole.id);
+
+    try {
+     
+    tomute.addRole(memberrole.id);
+
+    } catch(err) {
+    }
+
+    try {
+     
+    tomute.addRole(approle.id);
+    
+    } catch(err) {
+    }
+
+    try {
+     
+    tomute.addRole(recrole.id);
+        
+    } catch(err) {
+    }        
+
     geluktEmbed2 = new Discord.RichEmbed()
     .setColor("GREEN")
-    .setTitle(`${yes} **Jail Time Served!**`)
-    .setDescription(`You have successfully served your sentence of ${ms(ms(jailtime))} for ${jailReason}. You may now switch to a different voice channel.`)
+    .setTitle(`${yes} **Done!**`)
+    .setDescription(`<@${tomute.id}> has been ummuted.`)
+    .setFooter(`Mentioned User ID: ${tomute.id}`);
 
-    tojail.send(geluktEmbed2);
-}, ms(jailtime));
+    message.channel.send(geluktEmbed2);
+}, ms(mutetime));
 
-} catch(err) {
-    catchErr(err)
+let ModEmbed = new Discord.RichEmbed()
+.setTitle("**Command Used!**")
+.setTimestamp()
+.setColor("BLACK")
+.setDescription([
+    `**Command:** !tempmute`,
+    `**Used On:** <@${tomute.id}>`,
+    `**Used In:** ${message.channel}`,
+    `**Used By:** ${message.author.username}`,
+    `**Muted For:** ${mutetime}`
+  ].join('\n'))
+.setFooter(`Message ID: ${message.id} | Author ID: ${message.author.id}`);
 
-}
+let modlogchannel = message.guild.channels.find(x => x.name === 'modlog');
+modlogchannel.send({embed: ModEmbed});
+
+    } catch(err) {
+        catchErr(err)
+
+    }
 
 }
 
 module.exports.help = {
-name: "jail"
+    name: "tempmute"
 }
