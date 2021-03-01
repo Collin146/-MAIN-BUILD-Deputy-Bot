2116151c2bd722c6f04c7e8cd7e8c48f3776dc3e
Collin146 <47307903+Collin146@users.noreply.github.com>
2021-03-01 21:21:50 +0100

    Updated Patrol VC name definitions

--- index.js
+++ index.js
@@ -2050,7 +2050,8 @@ try {
     let RA1 = mainguild.channels.find(x => x.name === 'Ride Along #1');
     let RA2 = mainguild.channels.find(x => x.name === 'Ride Along #2');
     let RA3 = mainguild.channels.find(x => x.name === 'Ride Along #3');
-    let srutac = mainguild.channels.find(x => x.name === 'SRU Tactical Channel');
+    let srutac1 = mainguild.channels.find(x => x.name === 'Tac 1');
+    let srutac2 = mainguild.channels.find(x => x.name === 'Tac 2');
     let fireops = mainguild.channels.find(x => x.name === 'Fire Operations');
     let RTO = mainguild.channels.find(x => x.name === 'R.T.O.');
     let nine11 = mainguild.channels.find(x => x.name === '911 Centre');
@@ -2082,7 +2083,8 @@ try {
     if(newUserChannel.id === RA1.id) newMember.addRole(patrolrole.id); 
     if(newUserChannel.id === RA2.id) newMember.addRole(patrolrole.id); 
     if(newUserChannel.id === RA3.id) newMember.addRole(patrolrole.id); 
-    if(newUserChannel.id === srutac.id) newMember.addRole(patrolrole.id);
+    if(newUserChannel.id === srutac1.id) newMember.addRole(patrolrole.id);
+    if(newUserChannel.id === srutac2.id) newMember.addRole(patrolrole.id);
     if(newUserChannel.id === fireops.id) newMember.addRole(patrolrole.id); 
     if(newUserChannel.id === RTO.id) newMember.addRole(patrolrole.id); 
     if(newUserChannel.id === nine11.id) newMember.addRole(patrolrole.id); 
@@ -2114,7 +2116,8 @@ try {
     if(oldUserChannel.id === RA1.id) newMember.removeRole(patrolrole.id); 
     if(oldUserChannel.id === RA2.id) newMember.removeRole(patrolrole.id); 
     if(oldUserChannel.id === RA3.id) newMember.removeRole(patrolrole.id); 
-    if(oldUserChannel.id === srutac.id) newMember.removeRole(patrolrole.id); 
+    if(oldUserChannel.id === srutac1.id) newMember.removeRole(patrolrole.id);
+    if(oldUserChannel.id === srutac2.id) newMember.removeRole(patrolrole.id); 
     if(oldUserChannel.id === fireops.id) newMember.removeRole(patrolrole.id); 
     if(oldUserChannel.id === RTO.id) newMember.removeRole(patrolrole.id); 
     if(oldUserChannel.id === nine11.id) newMember.removeRole(patrolrole.id); 
@@ -2146,7 +2149,8 @@ try {
         if(newUserChannel.id === RA1.id) newMember.addRole(patrolrole.id);
         if(newUserChannel.id === RA2.id) newMember.addRole(patrolrole.id);
         if(newUserChannel.id === RA3.id) newMember.addRole(patrolrole.id);
-        if(newUserChannel.id === srutac.id) newMember.addRole(patrolrole.id);
+        if(newUserChannel.id === srutac1.id) newMember.addRole(patrolrole.id);
+        if(newUserChannel.id === srutac2.id) newMember.addRole(patrolrole.id);
         if(newUserChannel.id === fireops.id) newMember.addRole(patrolrole.id);
         if(newUserChannel.id === RTO.id) newMember.addRole(patrolrole.id);
         if(newUserChannel.id === nine11.id) newMember.addRole(patrolrole.id);
@@ -2176,7 +2180,8 @@ try {
     if(newUserChannel.id === RA1.id) return;
     if(newUserChannel.id === RA2.id) return;
     if(newUserChannel.id === RA3.id) return;
-    if(newUserChannel.id === srutac.id) return;
+    if(newUserChannel.id === srutac1.id) return;
+    if(newUserChannel.id === srutac2.id) return;
     if(newUserChannel.id === fireops.id) return;
     if(newUserChannel.id === RTO.id) return;
     if(newUserChannel.id === nine11.id) return;
